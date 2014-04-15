e710078b094301ac15c88d9365406c46696d60f9
Bertrand Néron <bneron@pasteur.fr>
2014-04-15 15:04:07 +0000

    set the licensing in all files

--- macsypy/config.py
+++ macsypy/config.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
-#===============================================================================
-# Created on Nov 12, 2012
-# 
-# @author: bertrand Néron
-# @contact: bneron <at> pasteur <dot> fr
-# @organization: organization_name
-# @license: license
-#===============================================================================
+################################################################################
+# MacSyFinder - Detection of macromolecular systems in protein datasets        #
+#               using systems modelling and similarity search.                 #
+# Authors: Sophie Abby, Bertrand Néron                                         #
+# Copyright © 2014  Institut Pasteur, Paris.                                   #
+# See the COPYRIGHT file for details                                           #
+#                                                                              #
+# MacsyFinder is distributed under the terms of the GNU General Public License #
+# (GPLv3). See the COPYING file for details.                                   #
+################################################################################
+
 
 import os
 import sys
--- macsypy/database.py
+++ macsypy/database.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
-#===============================================================================
-# Created on Feb 20, 2013
-# 
-# @author: bneron
-# @contact: bneron@pasteur.fr
-# @organization: Institut Pasteur
-# @license: license
-#===============================================================================
+################################################################################
+# MacSyFinder - Detection of macromolecular systems in protein datasets        #
+#               using systems modelling and similarity search.                 #
+# Authors: Sophie Abby, Bertrand Néron                                         #
+# Copyright © 2014  Institut Pasteur, Paris.                                   #
+# See the COPYRIGHT file for details                                           #
+#                                                                              #
+# MacsyFinder is distributed under the terms of the GNU General Public License #
+# (GPLv3). See the COPYING file for details.                                   #
+################################################################################
+
 
 
 
