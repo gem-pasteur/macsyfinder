f1fe231e8bb69828ab85a734aafba60770e28e8d
Bertrand Néron <bneron@pasteur.fr>
2014-07-02 11:05:24 +0200

    bump version to 1.0.RC2

--- setup.py
+++ setup.py
@@ -727,7 +727,7 @@ require_packages = []
 
 
 setup(name        = 'macsyfinder',
-      version     =  time.strftime("1.0"),
+      version     =  time.strftime("1.0.RC2"),
       description  = """MacSyFinder: Detection of macromolecular systems 
 in protein datasets using systems modelling and similarity search""",
       author  = "Sophie Abby, Bertrand Néron",
