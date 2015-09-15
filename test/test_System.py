623c618c2872b09cf53caca116d8eb5176f79da9
Bertrand Néron <bneron@pasteur.fr>
2015-09-15 15:17:19 +0200

    fix unit test to use ModelsLocation instead of profile_registry

--- test/test_System.py
+++ test/test_System.py
@@ -24,7 +24,7 @@ from macsypy.system import System
 from macsypy.gene import Gene
 from macsypy.gene import Homolog
 from macsypy.gene import Analog
-from macsypy.registries import ProfilesRegistry
+from macsypy.registries import ModelRegistry
 
 
 class Test(unittest.TestCase):
@@ -43,21 +43,22 @@ class Test(unittest.TestCase):
         log_handler = logging.FileHandler(log_file)
         macsy_log.addHandler(log_handler)
         
-        self.cfg = Config(sequence_db = os.path.join(self._data_dir, "base", "test_base.fa"),
-                          db_type = "gembase", 
-                          hmmer_exe = "",
-                           e_value_res = 1,
-                           i_evalue_sel = 0.5,
-                           def_dir = os.path.join(self._data_dir, 'DEF'),
-                           res_search_dir = tempfile.gettempdir(),
-                           res_search_suffix = "",
-                           profile_dir = os.path.join(self._data_dir, 'profiles'),
-                           profile_suffix = ".hmm",
-                           res_extract_suffix = "",
-                           log_level = 30,
-                           log_file = log_file
-                           )
-        self.profile_registry = ProfilesRegistry(self.cfg)
+        self.cfg = Config(hmmer_exe="",
+                         sequence_db=os.path.join(self._data_dir, "base", "test_base.fa"),
+                         db_type="gembase",
+                         e_value_res=1,
+                         i_evalue_sel=0.5,
+                         models_dir=os.path.join(self._data_dir, 'models'),
+                         res_search_dir=tempfile.gettempdir(),
+                         res_search_suffix="",
+                         profile_suffix=".hmm",
+                         res_extract_suffix="",
+                         log_level=30,
+                         log_file=log_file
+                         )
+        models_registry = ModelRegistry(self.cfg)
+        self.model_name = 'foo'
+        self.models_location = models_registry[self.model_name]
 
     def tearDown(self):
         # close loggers filehandles, so they don't block file deletion
@@ -70,10 +71,12 @@ class Test(unittest.TestCase):
         except:
             pass
 
+
     def test_name(self):
         name = 'foo'
         system = System(self.cfg, name, 10)
         self.assertEqual(system.name, name)
+
 
     def test_inter_gene_max_space(self):
         name = 'foo'
@@ -81,27 +84,30 @@ class Test(unittest.TestCase):
         system = System(self.cfg, name, inter_gene_max_space)
         self.assertEqual(system.inter_gene_max_space, inter_gene_max_space)
 
+
     def test_min_genes_required(self):
         name = 'foo'
         min_genes_required = 40
         system = System(self.cfg, name, 10, min_genes_required = min_genes_required)
-        gene = Gene(self.cfg, 'sctJ_FLG', system, self.profile_registry)
+        gene = Gene(self.cfg, 'sctJ_FLG', system, self.models_location)
         system.add_mandatory_gene( gene )
         self.assertEqual(system.min_genes_required, min_genes_required)
         #see https://projets.pasteur.fr/issues/1850
         system = System(self.cfg, name, 10)
         self.assertEqual(system.min_genes_required, len(system.mandatory_genes))
         
+        
     def test_min_mandatory_genes_required(self):
         name = 'foo'
         min_mandatory_genes_required = 40
         system = System(self.cfg, name, 10, min_mandatory_genes_required = min_mandatory_genes_required)
-        gene = Gene(self.cfg, 'sctJ_FLG', system, self.profile_registry)
+        gene = Gene(self.cfg, 'sctJ_FLG', system, self.models_location)
         system.add_mandatory_gene( gene )
         self.assertEqual(system.min_mandatory_genes_required, min_mandatory_genes_required)    
         #see https://projets.pasteur.fr/issues/1850
         system = System(self.cfg, name, 10)
         self.assertEqual(system.min_mandatory_genes_required, len(system.mandatory_genes))
+
 
     def test_max_nb_genes(self):
         name = 'foo'
@@ -124,52 +130,59 @@ class Test(unittest.TestCase):
         system = System(self.cfg, name, inter_gene_max_space)
         self.assertFalse(system.multi_loci)
 
+
     def test_add_mandatory_gene(self):
-        system = System("foo", self.cfg, 10)
-        gene = Gene(self.cfg, 'sctJ_FLG', system, self.profile_registry)
+        system = System(self.cfg, "foo", 10)
+        gene = Gene(self.cfg, 'sctJ_FLG', system, self.models_location)
         system.add_mandatory_gene(gene)
         self.assertEqual(system._mandatory_genes, [gene])
         self.assertEqual(system._accessory_genes, [])
         self.assertEqual(system._forbidden_genes, [])
 
+
     def test_add_accessory_gene(self):
         system = System(self.cfg, "foo", 10)
-        gene = Gene(self.cfg, 'sctJ_FLG', system, self.profile_registry)
+        gene = Gene(self.cfg, 'sctJ_FLG', system, self.models_location)
         system.add_accessory_gene(gene)
         self.assertEqual(system._accessory_genes, [gene])
         self.assertEqual(system._mandatory_genes, [])
         self.assertEqual(system._forbidden_genes, [])
 
+
     def test_add_forbidden_gene(self):
         system = System(self.cfg, "foo", 10)
-        gene = Gene(self.cfg, 'sctJ_FLG', system, self.profile_registry)
+        gene = Gene(self.cfg, 'sctJ_FLG', system, self.models_location)
         system.add_forbidden_gene(gene)
         self.assertEqual(system._forbidden_genes, [gene])
         self.assertEqual(system._accessory_genes, [])
         self.assertEqual(system._mandatory_genes, [])
 
+
     def test_mandatory_genes(self):
         system = System(self.cfg, "foo", 10)
-        gene = Gene(self.cfg, 'sctJ_FLG', system, self.profile_registry)
+        gene = Gene(self.cfg, 'sctJ_FLG', system, self.models_location)
         system.add_mandatory_gene(gene)
         self.assertEqual(system.mandatory_genes, [gene])
 
+
     def test_accessory_genes(self):
         system = System(self.cfg, "foo", 10)
-        gene = Gene(self.cfg, 'sctJ_FLG', system, self.profile_registry)
+        gene = Gene(self.cfg, 'sctJ_FLG', system, self.models_location)
         system.add_accessory_gene(gene)
         self.assertEqual(system.accessory_genes, [gene])
 
+
     def test_forbidden_genes(self):
         system = System(self.cfg, "foo", 10)
-        gene = Gene(self.cfg, 'sctJ_FLG', system, self.profile_registry)
+        gene = Gene(self.cfg, 'sctJ_FLG', system, self.models_location)
         system.add_forbidden_gene(gene)
         self.assertEqual(system.forbidden_genes, [gene])
+
 
     def test_get_gene(self):
         system = System(self.cfg, "foo", 10)
         gene_name = 'sctJ_FLG'
-        gene = Gene(self.cfg, gene_name, system, self.profile_registry)
+        gene = Gene(self.cfg, gene_name, system, self.models_location)
         for meth in (system.add_forbidden_gene, system.add_accessory_gene, system.add_mandatory_gene):
             system._mandatory_genes = []
             system._accessory_genes = []
@@ -180,7 +193,7 @@ class Test(unittest.TestCase):
         self.assertRaises(KeyError, system.get_gene, 'bar')
 
         homolog_name = 'sctJ'
-        gene_homolog = Gene(self.cfg, homolog_name, system, self.profile_registry)
+        gene_homolog = Gene(self.cfg, homolog_name, system, self.models_location)
         homolog = Homolog(gene_homolog, gene)
         gene.add_homolog(homolog)
         for meth in (system.add_forbidden_gene, system.add_accessory_gene, system.add_mandatory_gene):
@@ -191,7 +204,7 @@ class Test(unittest.TestCase):
             self.assertEqual(homolog, system.get_gene(homolog_name))
 
         analog_name = 'sctC'
-        gene_analog = Gene(self.cfg, analog_name, system, self.profile_registry)
+        gene_analog = Gene(self.cfg, analog_name, system, self.models_location)
         analog = Analog(gene_analog, gene)
         gene.add_analog(analog)
         for meth in (system.add_forbidden_gene, system.add_accessory_gene, system.add_mandatory_gene):
@@ -201,12 +214,13 @@ class Test(unittest.TestCase):
             meth(gene)
             self.assertEqual(analog, system.get_gene(analog_name))
 
+
     def test_get_gene_ref(self):
         system = System(self.cfg, "foo", 10)
         gene_name = 'sctJ_FLG'
-        gene_ref = Gene(self.cfg, gene_name, system, self.profile_registry)
+        gene_ref = Gene(self.cfg, gene_name, system, self.models_location)
         homolog_name = 'sctJ'
-        gene_homolg = Gene(self.cfg, homolog_name, system, self.profile_registry)
+        gene_homolg = Gene(self.cfg, homolog_name, system, self.models_location)
         homolog = Homolog(gene_homolg, gene_ref)
         gene_ref.add_homolog(homolog)
 
@@ -217,6 +231,6 @@ class Test(unittest.TestCase):
             meth(gene_ref)
             self.assertEqual(gene_ref, system.get_gene_ref(homolog))
         self.assertIsNone(system.get_gene_ref(gene_ref))
-        gene_ukn = Gene(self.cfg, 'abc', system, self.profile_registry)
+        gene_ukn = Gene(self.cfg, 'abc', system, self.models_location)
         self.assertRaises(KeyError, system.get_gene_ref, gene_ukn)
         
--- test/test_SystemBank.py
+++ test/test_SystemBank.py
@@ -41,20 +41,20 @@ class Test(unittest.TestCase):
         log_handler = logging.FileHandler(log_file)
         macsy_log.addHandler(log_handler)
         
-        self.cfg = Config( sequence_db = os.path.join(self._data_dir, "base", "test_base.fa"),
-                           db_type = "gembase",
-                           hmmer_exe = "",
-                           e_value_res = 1,
-                           i_evalue_sel = 0.5,
-                           def_dir = os.path.join(self._data_dir, 'DEF'),
-                           res_search_dir = tempfile.gettempdir(),
-                           res_search_suffix = "",
-                           profile_dir = os.path.join(self._data_dir, 'profiles'),
-                           profile_suffix = ".hmm",
-                           res_extract_suffix = "",
-                           log_level = 30,
-                           log_file = log_file
-                           )
+        self.cfg = Config(hmmer_exe="",
+                          sequence_db=os.path.join(self._data_dir, "base", "test_base.fa"),
+                          db_type="gembase",
+                          e_value_res=1,
+                          i_evalue_sel=0.5,
+                          models_dir=os.path.join(self._data_dir, 'models'),
+                          res_search_dir=tempfile.gettempdir(),
+                          res_search_suffix="",
+                          profile_suffix=".hmm",
+                          res_extract_suffix="",
+                          log_level=30,
+                          log_file=log_file
+                          )
+
 
     def tearDown(self):
         # close loggers filehandles, so they don't block file deletion
@@ -68,19 +68,19 @@ class Test(unittest.TestCase):
             pass
 
     def test_add_get_system(self):
-        system_name= 'foo'
-        self.assertRaises(KeyError, system_bank.__getitem__, system_name)
-        system_foo = System(self.cfg, system_name, 10)
+        model_name = 'foo'
+        self.assertRaises(KeyError, system_bank.__getitem__, model_name)
+        system_foo = System(self.cfg, model_name, 10)
         system_bank.add_system(system_foo)
-        self.assertTrue( isinstance( system_foo, System ))
-        self.assertEqual( system_foo, system_bank[system_name] )
+        self.assertTrue(isinstance(system_foo, System))
+        self.assertEqual(system_foo, system_bank[model_name])
 
     def test_contains(self):
         system_in = System(self.cfg, "foo", 10)
         system_bank.add_system(system_in)
         self.assertIn(system_in, system_bank)
         system_out = System(self.cfg, "bar", 10)
-        self.assertNotIn( system_out, system_bank)
+        self.assertNotIn(system_out, system_bank)
 
     def test_iter(self):
         systems = [System(self.cfg, 'foo', 10), System(self.cfg, 'bar', 10)]
@@ -89,7 +89,7 @@ class Test(unittest.TestCase):
         i = 0
         for s in system_bank:
             self.assertIn(s, systems)
-            i = i + 1
+            i += 1
         self.assertEqual(i, len(systems))
 
     def test_get_uniq_object(self):
@@ -97,5 +97,5 @@ class Test(unittest.TestCase):
         system_bank.add_system(system_foo)
         system_1 = system_bank[system_foo.name]
         system_2 = system_bank[system_foo.name]
-        self.assertEqual( system_1, system_2 )
+        self.assertEqual(system_1, system_2)
         
