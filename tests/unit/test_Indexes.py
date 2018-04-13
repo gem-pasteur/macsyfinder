6f0dd8064509be8562988e32b4999b0be71e660d
Bertrand Néron <bneron@pasteur.fr>
2018-04-13 15:06:35 +0200

    :fire: remove code related to makeblastdb

--- tests/unit/test_Indexes.py
+++ tests/unit/test_Indexes.py
@@ -76,89 +76,6 @@ class Test(MacsyTest):
         except:
             pass
 
-
-    def test_find_hmmer_indexes_no_files(self):
-        idx = Indexes(self.cfg)
-        # tester pas de fichier
-        hmmer_idx = idx.find_hmmer_indexes()
-        self.assertListEqual(hmmer_idx, [])
-
-
-    def test_find_hmmer_indexes_all_files(self):
-        idx = Indexes(self.cfg)
-        suffixes = ('.phr', '.pin', '.psd', '.psi', '.psq')
-        files_2_find = []
-        for s in suffixes:
-            new_idx = os.path.join(self.cfg.sequence_db + s)
-            open(new_idx, 'w')
-            files_2_find.append(new_idx)
-        hmmer_idx = idx.find_hmmer_indexes()
-        self.assertListEqual(hmmer_idx, files_2_find)
-
-
-    def test_find_hmmer_indexes_all_files_and_pal(self):
-        idx = Indexes(self.cfg)
-        # tester tous les fichiers + pal
-        suffixes = ('.phr', '.pin', '.psd', '.psi', '.psq', '.pal')
-        for s in suffixes:
-            new_idx = os.path.join(self.cfg.sequence_db + s)
-            open(new_idx, 'w')
-        self.assertRaises(RuntimeError, idx.find_hmmer_indexes)
-
-
-    def test_find_hmmer_indexes_some_files(self):
-        idx = Indexes(self.cfg)
-        # tester pas tous les fichiers
-        suffixes = ('.phr', '.pin', '.psd', '.psi')
-        for s in suffixes:
-            new_idx = os.path.join(self.cfg.sequence_db + s)
-            open(new_idx, 'w')
-        self.assertRaises(RuntimeError, idx.find_hmmer_indexes)
-
-
-    def test_find_hmmer_indexes_lack_pal(self):
-        idx = Indexes(self.cfg)
-        # tester plusieurs index pas de pal
-        suffixes = ('.phr', '.pin', '.psd', '.psi', '.psq')
-        for s in suffixes:
-            for i in range(2):
-                new_idx = os.path.join(self.cfg.sequence_db + str(i) + s)
-                open(new_idx, 'w')
-        self.assertRaises(RuntimeError, idx.find_hmmer_indexes)
-
-
-    def test_find_hmmer_indexes_all_files_and_2virtual(self):
-        idx = Indexes(self.cfg)
-        # tester 1 fichier index + pal
-        suffixes = ('.phr', '.pin', '.psd', '.psi', '.psq', '.pal')
-        files_2_find = []
-        for s in suffixes:
-            for i in range(2):
-                new_idx = os.path.join(self.cfg.sequence_db + str(i) + s)
-                open(new_idx, 'w')
-                files_2_find.append(new_idx)
-        self.assertRaises(RuntimeError, idx.find_hmmer_indexes)
-
-
-    def test_find_hmmer_indexes_all_files_and_virtual(self):
-        idx = Indexes(self.cfg)
-        # tester index + pal
-        suffixes = ('.phr', '.pin', '.psd', '.psi', '.psq')
-        files_2_find = []
-        for s in suffixes:
-            for i in range(2):
-                new_idx = os.path.join("{0}.{1:d}.{2}".format(self.cfg.sequence_db, i, s))
-                open(new_idx, 'w')
-                files_2_find.append(new_idx)
-        new_idx = os.path.join(self.cfg.sequence_db + '.pal')
-        open(new_idx, 'w')
-        files_2_find.append(new_idx)
-        files_2_find.sort()
-        hmmer_idx = idx.find_hmmer_indexes()
-        hmmer_idx.sort()
-        self.assertListEqual(hmmer_idx, files_2_find)
-
-
     def test_find_my_indexes(self):
         idx = Indexes(self.cfg)
         self.assertIsNone(idx.find_my_indexes())
@@ -166,52 +83,28 @@ class Test(MacsyTest):
         open(new_idx, 'w')
         self.assertEqual(idx.find_my_indexes(), new_idx)
 
-    @unittest.skipIf(not (which('makeblastdb') or which('formatdb')), 'neither makeblast nor formatdb found in PATH')
     def test_build_no_idx(self):
-        if not which('makeblastdb') and which('formatdb'):
-            self.cfg.options['index_db_exe'] = 'formatdb'
         idx = Indexes(self.cfg)
         idx.build()
         my_idx = idx.find_my_indexes()
-        hmmer_idx = idx.find_hmmer_indexes()
         self.assertEqual(my_idx, os.path.join(os.path.dirname(self.cfg.sequence_db), idx.name + ".idx"))
-        self.assertEqual(hmmer_idx, [self.cfg.sequence_db + suffix for suffix in ('.phr', '.pin', '.psd', '.psi', '.psq')])
-        
 
-    @unittest.skipIf(not (which('makeblastdb') or which('formatdb')), 'neither makeblast nor formatdb found in PATH')
+
     def test_build_with_idx(self):
-        if not which('makeblastdb') and which('formatdb'):
-            self.cfg.options['index_db_exe'] = 'formatdb'
-        # put fake hmmer indexes
-        suffixes = ('.phr', '.pin', '.psd', '.psi', '.psq')
-        for s in suffixes:
-            new_idx = os.path.join(self.cfg.sequence_db + s)
-            open(new_idx, 'w')
         idx = Indexes(self.cfg)
-        new_idx = open(os.path.join( os.path.dirname(self.cfg.sequence_db), idx.name + ".idx"), 'w')
+        open(os.path.join(os.path.dirname(self.cfg.sequence_db), idx.name + ".idx"), 'w').close()
         idx.build()
         my_idx = idx.find_my_indexes()
-        hmmer_idx = idx.find_hmmer_indexes()
-        for f in hmmer_idx + [my_idx]:
-            self.assertEqual(os.path.getsize(f), 0)
+        self.assertEqual(os.path.getsize(my_idx), 0)
 
-    @unittest.skipIf(not (which('makeblastdb') or which('formatdb')), 'neither makeblast nor formatdb found in PATH')
+
     def test_build_force(self):
-        # put fake hmmer indexes
-        if not which('makeblastdb') and which('formatdb'):
-            self.cfg.options['index_db_exe'] = 'formatdb'
-       
-        suffixes = ('.phr', '.pin', '.psd', '.psi', '.psq')
-        for s in suffixes:
-            new_idx = os.path.join(self.cfg.sequence_db + s)
-            open(new_idx, 'w')
         idx = Indexes(self.cfg)
         idx.build(force=True)
         my_idx = idx.find_my_indexes()
-        hmmer_idx = idx.find_hmmer_indexes()
-        for f in hmmer_idx + [my_idx]:
-            self.assertNotEqual(os.path.getsize(f), 0)
-            
+        self.assertNotEqual(os.path.getsize(my_idx), 0)
+
+
     @unittest.skipIf(platform.system() == 'Windows' or os.getuid() == 0, 'Skip test on Windows or if run as root')
     def test_build_not_writable(self):
         # Skip test on Windows, since setting the folder permissions is not affecting files inside
