29a808ad78dada7a66bd5dd1b77eebb50845f816
Jerome Raciazek <jerome.raciazek@pasteur.fr>
2018-06-13 20:18:45 +0200

    Fix regression in SystemOccurence.state unit test.

--- tests/unit/test_SystemOccurence.py
+++ tests/unit/test_SystemOccurence.py
@@ -23,7 +23,7 @@ from macsypy.config import Config
 from macsypy.gene import Gene
 from macsypy.system import System
 from macsypy.search_systems import SystemOccurence
-from macsypy.registries import ProfilesRegistry
+from macsypy.registries import ModelRegistry
 from tests import MacsyTest
 
 
@@ -36,7 +36,7 @@ class Test(MacsyTest):
         # add only one handler to the macsypy logger
         from macsypy.report import _log
         macsy_log = _log.parent
-        log_file = 'NUL' if platform.system() == 'Windows' else '/dev/null'
+        log_file = os.devnull
         log_handler = logging.FileHandler(log_file)
         macsy_log.addHandler(log_handler)
 
@@ -45,17 +45,19 @@ class Test(MacsyTest):
                           db_type="gembase",
                           e_value_res=1,
                           i_evalue_sel=0.5,
-                          def_dir=os.path.join(self._data_dir, 'DEF'),
                           res_search_dir=tempfile.gettempdir(),
                           res_search_suffix="",
-                          profile_dir=os.path.join(self._data_dir, 'profiles'),
                           profile_suffix=".hmm",
                           res_extract_suffix="",
                           log_level=30,
+                          models_dir=os.path.join(self._data_dir, 'models'),
                           log_file=log_file
                          )
-        self.profile_registry = ProfilesRegistry(self.cfg)
+
   
+        models_registry = ModelRegistry(self.cfg)
+        self.model_name = 'foo'
+        self.models_location = models_registry[self.model_name]
     def tearDown(self):
         # close loggers filehandles, so they don't block file deletion
         # in shutil.rmtree calls in Windows
