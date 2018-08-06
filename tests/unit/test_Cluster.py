5dc8c71ed1ba794e9b4955ed63f2585e06cd61af
Jerome Raciazek <jerome.raciazek@pasteur.fr>
2018-08-06 17:38:24 +0200

    Add Cluster.__len__ unit test.

---
+++ tests/unit/test_Cluster.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+# MacSyFinder - Detection of macromolecular systems in protein datasets        #
+#               using systems modelling and similarity search.                 #
+# Authors: Sophie Abby, Bertrand Néron                                         #
+# Copyright © 2014  Institut Pasteur, Paris.                                   #
+# See the COPYRIGHT file for details                                           #
+#                                                                              #
+# MacsyFinder is distributed under the terms of the GNU General Public License #
+# (GPLv3). See the COPYING file for details.                                   #
+################################################################################
+
+
+import os
+import shutil
+import tempfile
+import logging
+from macsypy.config import Config
+from macsypy.system import System
+from macsypy.report import Hit
+from macsypy.search_systems import Cluster
+from macsypy.registries import ModelRegistry
+from macsypy.database import Indexes
+from tests import MacsyTest
+
+
+class Test(MacsyTest):
+
+    def setUp(self):
+        logger = logging.getLogger()
+        logger.manager.loggerDict.clear()
+
+        # add only one handler to the macsypy logger
+        from macsypy.report import _log
+        macsy_log = _log.parent
+        log_file = os.devnull
+        log_handler = logging.FileHandler(log_file)
+        macsy_log.addHandler(log_handler)
+
+        self.cfg = Config(hmmer_exe="hmmsearch",
+                          sequence_db=self.find_data("base", "test_base.fa"),
+                          db_type="gembase",
+                          e_value_res=1,
+                          i_evalue_sel=0.5,
+                          res_search_dir=tempfile.gettempdir(),
+                          res_search_suffix=".search_hmm.out",
+                          profile_suffix=".hmm",
+                          res_extract_suffix="",
+                          log_level=30,
+                          models_dir=self.find_data('models'),
+                          log_file=log_file)
+
+        shutil.copy(self.cfg.sequence_db, self.cfg.working_dir)
+        self.cfg.options['sequence_db'] = os.path.join(self.cfg.working_dir, os.path.basename(self.cfg.sequence_db))
+
+        idx = Indexes(self.cfg)
+        idx._build_my_indexes()
+
+        models_registry = ModelRegistry(self.cfg)
+        self.model_name = 'foo'
+        self.models_location = models_registry[self.model_name]
+
+    def tearDown(self):
+        # close loggers filehandles, so they don't block file deletion
+        # in shutil.rmtree calls in Windows
+        logging.shutdown()
+        logger = logging.getLogger()
+        logger.manager.loggerDict.clear()
+        try:
+            shutil.rmtree(self.cfg.working_dir)
+        except:
+            pass
+
+    def test_len(self):
+        system = System(self.cfg, 'foo', 10)
+        cluster = Cluster(system)
+        li = [None] * 12
+        hit = Hit(*li)
+        cluster.hits = [hit]
+        self.assertEqual(len(cluster), 1)
