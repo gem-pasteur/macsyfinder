5685863de9d66c9550e23bb644409e263e3e0d87
Jerome Raciazek <jerome.raciazek@pasteur.fr>
2018-08-08 20:18:02 +0200

    Add Cluster.save unit test.

--- tests/unit/test_Cluster.py
+++ tests/unit/test_Cluster.py
@@ -159,8 +159,7 @@ class Test(MacsyTest):
 
         h1, h2, h3 = all_hits[:3]
 
-        system = System(self.cfg, 'foo', 4)
-        cluster = Cluster(system)
+        cluster = Cluster([system])
 
         cluster.add(h1)
         self.assertEqual(cluster.begin, 505)
@@ -179,6 +178,131 @@ class Test(MacsyTest):
             cluster.add(h3)
         self.assertEqual(context.exception.message,
                          "Attempting to gather in a cluster hits from different replicons ! ")
+
+        try:
+            shutil.rmtree(out_dir)
+        except:
+            pass
+
+    def test_save(self):
+
+        out_dir = "/tmp/macsyfinder-test_fill_with_cluster-" + strftime("%Y%m%d_%H-%M-%S")
+
+        # for this test, we use a specific configuration with a dedicated
+        # working directory (i.e. we don't use the generic configuration
+        # defined in setUp() method).
+        config = Config(hmmer_exe="hmmsearch",
+                        out_dir=out_dir,
+                        db_type="gembase",
+                        previous_run="tests/data/data_set_1/complete_run_results",
+                        e_value_res=1,
+                        i_evalue_sel=0.5,
+                        res_search_suffix=".search_hmm.out",
+                        profile_suffix=".hmm",
+                        res_extract_suffix="",
+                        log_level=30,
+                        models_dir="tests/data/data_set_1/models",
+                        log_file=os.devnull)
+
+        idx = Indexes(config)
+        idx._build_my_indexes()
+
+        parser = SystemParser(config, system_bank, gene_bank)
+        parser.parse(['set_1/T9SS'])
+
+        system_1 = system_bank['set_1/T9SS']
+
+        def get_hits(system):
+            genes = system.mandatory_genes + system.accessory_genes + system.forbidden_genes
+
+            ex_genes = []
+            for g in genes:
+                if g.exchangeable:
+                    h_s = g.get_homologs()
+                    ex_genes += h_s
+                    a_s = g.get_analogs()
+                    ex_genes += a_s
+            all_genes = (genes + ex_genes)
+
+            all_reports = search_genes(all_genes, config)
+
+            all_hits = [hit for subl in [report.hits for report in all_reports] for hit in subl]
+
+            all_hits = sorted(all_hits, key=attrgetter('score'), reverse=True)
+            all_hits = sorted(all_hits, key=attrgetter('replicon_name', 'position'))
+
+            return all_hits
+
+        all_hits_1 = get_hits(system_1)
+
+        # debug
+        # print [h.gene.name for h in all_hits_1]
+
+        # debug
+        # print cluster.systems
+        # print cluster._putative_system
+        # print cluster._compatible_systems
+        # print cluster._state
+
+        # debug
+        # fqn = 'set_1/T4P'
+        # fqn = 'set_1/CONJ'
+        # fqn = 'set_1/Tad'
+        # fqn = 'set_1/Flagellum'
+        # fqn = 'set_1/T2SS'
+
+        # test case 1
+
+        cluster = Cluster([system_1])
+        for h in all_hits_1:
+            cluster.add(h)
+        cluster.save()
+        self.assertEqual(cluster.systems, {'set_1/T9SS': 17})
+        self.assertEqual(cluster._putative_system, 'set_1/T9SS')
+        self.assertEqual(cluster._compatible_systems, ['set_1/T9SS'])
+        self.assertEqual(cluster._state, 'clear')
+
+        # test case 2
+
+        cluster = Cluster([system_1])
+        cluster.add(all_hits_1[0])
+        cluster.hits[0].gene._loner = False
+        cluster.save()
+        self.assertEqual(cluster.systems, {'set_1/T9SS': 1})
+        self.assertEqual(cluster._putative_system, 'set_1/T9SS')
+        self.assertEqual(cluster._compatible_systems, [])
+        self.assertEqual(cluster._state, 'ineligible')
+
+        # test case 3
+
+        fqn = 'set_1/T3SS'
+        parser.parse([fqn])
+        system_2 = system_bank[fqn]
+        all_hits_2 = get_hits(system_2)
+        cluster = Cluster([system_1, system_2])
+        for h in all_hits_1 + all_hits_2:
+            cluster.add(h)
+        cluster.save()
+        self.assertEqual(cluster.systems, {'set_1/T9SS': 17, 'set_1/T3SS': 2})
+        self.assertEqual(cluster._putative_system, 'set_1/T3SS')
+        self.assertEqual(cluster._compatible_systems, [])
+        self.assertEqual(cluster._state, 'ambiguous')
+
+        # test case 4
+
+        """
+        FIXME
+
+        Warning: some parts of the 'save()' method are not tested.  To test
+        those parts, the try_system() sub-func must return "clear", which seems
+        not possible to achieve with the test dataset used.
+
+        DRAFT
+        cluster = Cluster([system_1, system_2])
+        for h in all_hits_1 + all_hits_2:
+            cluster.add(h)
+        cluster.save()
+        """
 
         try:
             shutil.rmtree(out_dir)
