cc9b2076af5f3c656a40a57399c0d11b637bc8cb
Jerome Raciazek <jerome.raciazek@pasteur.fr>
2018-08-25 19:13:09 +0200

    Move test env at method level. Remove unused variable.
    Move test env at method level as different test env will
    be needed to improve tests of search_systems module.

--- tests/unit/test_search_systems.py
+++ tests/unit/test_search_systems.py
@@ -25,16 +25,12 @@ class Test(MacsyTest):
 
     def setUp(self):
         self.macsy_test_env = MacsyTestEnv()
-        self.macsy_test_env.load("env_003")
-
-        self.test_dir = tempfile.mkdtemp()
 
     def tearDown(self):
-        self.macsy_test_env.unload("env_003")
-
-        shutil.rmtree(self.test_dir)
+        self.macsy_test_env = None
 
     def test_build_clusters(self):
+        self.macsy_test_env.load("env_003")
 
         # case 1
 
@@ -63,11 +59,19 @@ class Test(MacsyTest):
             #print "Recap clusters"
         """
 
+        self.macsy_test_env.unload("env_003")
+
     def test_get_compatible_systems(self):
+        self.macsy_test_env.load("env_003")
+
         inter = get_compatible_systems([1, 2, 3], [3, 4])
         self.assertEqual(inter, [3])
 
+        self.macsy_test_env.unload("env_003")
+
     def test_get_best_hits(self):
+        self.macsy_test_env.load("env_003")
+
         hits = self.macsy_test_env.all_hits[0:2]
 
         hits[0].position = hits[1].position
@@ -93,7 +97,11 @@ class Test(MacsyTest):
         best_hits = get_best_hits(hits, criterion="profile_coverage")
         self.assertEqual(best_hits[0].gene.name, "T9SS_gldN_TIGR03523")
 
+        self.macsy_test_env.unload("env_003")
+
     def test_disambiguate_cluster(self):
+        self.macsy_test_env.load("env_003")
+
         rep_db = RepliconDB(self.macsy_test_env.cfg)
         rep_info = rep_db['AESU001c01a']
         (clusters, multi_syst_genes) = build_clusters(self.macsy_test_env.all_hits, [self.macsy_test_env.system], rep_info)
@@ -113,7 +121,11 @@ class Test(MacsyTest):
         Deal with "accessory foreign genes",
         """
 
+        self.macsy_test_env.unload("env_003")
+
     def test_analyze_clusters_replicon(self):
+        self.macsy_test_env.load("env_003")
+
         rep_db = RepliconDB(self.macsy_test_env.cfg)
         rep_info = rep_db['AESU001c01a']
         (clusters, multi_syst_genes) = build_clusters(self.macsy_test_env.all_hits, [self.macsy_test_env.system], rep_info)
@@ -123,7 +135,11 @@ class Test(MacsyTest):
         # FIXME
         # many part of analyze_clusters_replicon func not tested
 
+        self.macsy_test_env.unload("env_003")
+
     def test_search_systems(self):
+        self.macsy_test_env.load("env_003")
+
         search_systems(self.macsy_test_env.all_hits, [self.macsy_test_env.system], self.macsy_test_env.cfg)
 
         tabfilename = os.path.join(self.macsy_test_env.cfg.working_dir, 'macsyfinder.tab')
@@ -142,3 +158,5 @@ class Test(MacsyTest):
 
         # FIXME
         # many part of search_systems func not tested
+
+        self.macsy_test_env.unload("env_003")
