db8cf7b965a9d428ba7004034d80f73b8c93aed8
Jerome Raciazek <jerome.raciazek@pasteur.fr>
2018-08-27 21:02:11 +0200

    Remove duplicate code.

--- tests/unit/test_Cluster.py
+++ tests/unit/test_Cluster.py
@@ -34,55 +34,15 @@ from tests.unit import MacsyTestEnv
 class Test(MacsyTest):
 
     def setUp(self):
-        logger = logging.getLogger()
-        logger.manager.loggerDict.clear()
-
-        # add only one handler to the macsypy logger
-        from macsypy.report import _log
-        macsy_log = _log.parent
-        log_file = os.devnull
-        log_handler = logging.FileHandler(log_file)
-        macsy_log.addHandler(log_handler)
-
-        self.cfg = Config(hmmer_exe="hmmsearch",
-                          sequence_db=self.find_data("base", "test_base.fa"),
-                          db_type="gembase",
-                          e_value_res=1,
-                          i_evalue_sel=0.5,
-                          res_search_dir=tempfile.gettempdir(),
-                          res_search_suffix=".search_hmm.out",
-                          profile_suffix=".hmm",
-                          res_extract_suffix="",
-                          log_level=30,
-                          models_dir=self.find_data('models'),
-                          log_file=log_file)
-
-        shutil.copy(self.cfg.sequence_db, self.cfg.working_dir)
-        self.cfg.options['sequence_db'] = os.path.join(self.cfg.working_dir, os.path.basename(self.cfg.sequence_db))
-
-        idx = Indexes(self.cfg)
-        idx._build_my_indexes()
-
-        models_registry = ModelRegistry(self.cfg)
-        self.model_name = 'foo'
-        self.models_location = models_registry[self.model_name]
+        self.macsy_test_env = MacsyTestEnv()
+        self.macsy_test_env.load("env_001")
 
     def tearDown(self):
-        # close loggers filehandles, so they don't block file deletion
-        # in shutil.rmtree calls in Windows
-        logging.shutdown()
-        logger = logging.getLogger()
-        logger.manager.loggerDict.clear()
-        try:
-            shutil.rmtree(self.cfg.working_dir)
-        except:
-            pass
-
-        system_bank._system_bank = {}
-        gene_bank._genes_bank = {}
+        self.macsy_test_env.unload("env_001")
+        self.macsy_test_env = None
 
     def test_len(self):
-        system = System(self.cfg, 'foo', 10)
+        system = System(self.macsy_test_env.cfg, 'foo', 10)
         cluster = Cluster(system)
         li = [None] * 12
         hit = Hit(*li)
@@ -91,20 +51,20 @@ class Test(MacsyTest):
 
     def test_putative_system(self):
         system_name = 'set_1/T9SS'
-        system = System(self.cfg, system_name, 10)
+        system = System(self.macsy_test_env.cfg, system_name, 10)
         cluster = Cluster(system)
         cluster._putative_system = system_name
         self.assertEqual(cluster.putative_system, system_name)
 
     def test_compatible_systems(self):
-        system = System(self.cfg, 'set_1/T9SS', 10)
+        system = System(self.macsy_test_env.cfg, 'set_1/T9SS', 10)
         cluster = Cluster(system)
         compatible_system_name = 'set_1/T2SS'
         cluster._compatible_systems.append(compatible_system_name)
         self.assertListEqual(cluster.compatible_systems, [compatible_system_name])
 
     def test_state(self):
-        system = System(self.cfg, 'foo', 4)
+        system = System(self.macsy_test_env.cfg, 'foo', 4)
         cluster = Cluster(system)
         state = cluster.state
         self.assertEqual(state, '')
