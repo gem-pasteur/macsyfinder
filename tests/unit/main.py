00ef83342509c605aa6b1da675e56244bda07878
Bertrand <bneron@pasteur.fr>
2018-10-17 13:12:55 +0200

    :art: fix pep8 for tests/unit/main.py

--- tests/unit/main.py
+++ tests/unit/main.py
@@ -51,7 +51,6 @@ def run(lib, tests, verbosity=0):
     return res
 
 
-
 if __name__ == '__main__':
 
     if 'MACSY_HOME' in os.environ:
@@ -75,4 +74,3 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     res = run(MACSY_HOME, args.tests, args.verbosity)
-    
