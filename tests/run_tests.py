a9abad2e1ed0723247009dbbb61f1202bc6bee43
Bertrand <bneron@pasteur.fr>
2018-12-18 13:35:38 +0100

    :sparkles: update run_tests
    to be usable without to define MACSY_HOME
    to be usable from setup.py

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -18,50 +18,56 @@ import sys
 import unittest
 
 
-def run(lib, tests, verbosity=0):
-    """
-    run the unit tests and print the results on stderr
-    
-    :param lib: the path where is the macsypy
-    :type lib: string
-    :param tests: the name of test to run. if tests is empty list, discover recursively tests form this directory.
-                  a test is python module with the test_*.py pattern
-    :type tests: list of string
-    :param verbosity: the verbosity of the output
-    :type verbosity: int
-    """
-    if lib not in sys.path:
-        sys.path.insert(0, lib)
-    if not tests:
-        suite = unittest.TestLoader().discover(os.path.dirname(__file__), pattern="test_*.py")
+def discover(test_files=None, test_root_path=None):
+    if not test_root_path:
+        test_root_path = os.path.dirname(__file__)
+
+    if not test_files:
+        suite = unittest.TestLoader().discover(test_root_path, pattern="test_*.py")
+
     else:
+        test_files = [os.path.abspath(f) for f in test_files]
+        test_files = [t for t in test_files if test_root_path in t]
         suite = unittest.TestSuite()
-        for test in tests: 
-            if os.path.exists(test):
-                if os.path.isfile(test):
-                    fpath, fname = os.path.split(test)
+        for test_file in test_files:
+            if os.path.exists(test_file):
+                if os.path.isfile(test_file):
+                    fpath, fname = os.path.split(test_file)
                     suite.addTests(unittest.TestLoader().discover(fpath, pattern=fname))
-                elif os.path.isdir(test):  
-                    suite.addTests(unittest.TestLoader().discover(test)) 
+                elif os.path.isdir(test_file):
+                    suite.addTests(unittest.TestLoader().discover(test_file))
             else:
-                sys.stderr.write("{0} : no such file or directory\n".format(test))
+                sys.stderr.write("{0} : no such file or directory\n".format(test_file))
 
-    res = unittest.TextTestRunner(verbosity=verbosity).run(suite)
-    return res
+    return suite
 
 
-if __name__ == '__main__':
+def run_tests(test_files, verbosity=0):
+    """
+    Execute Unit Tests
+    :param test_files: the file names of tests to run.
+    of it is empty, discover recursively tests from 'tests' directory.
+    a test is python module with the test_*.py pattern
+    :type test_files: list of string
+    :param verbosity: the verbosity of the output
+    :type verbosity: positive int
+    :return: True if the test passed successfully, False otherwise.
+    :rtype: bool
+    """
+    test_root_path = os.path.abspath(os.path.dirname(__file__))
+    suite = discover(test_files, test_root_path)
+    test_runner = unittest.TextTestRunner(verbosity=verbosity).run(suite)
+    return test_runner
 
-    if 'MACSY_HOME' in os.environ:
-        MACSY_HOME = os.environ['MACSY_HOME']
-    else:
-        MACSY_HOME = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
     from argparse import ArgumentParser
     parser = ArgumentParser()
     parser.add_argument("tests",
                         nargs='*',
-                        default=[],
+                        default=False,
                         help="name of test to execute")
 
     parser.add_argument("-v", "--verbose",
@@ -71,5 +77,27 @@ if __name__ == '__main__':
                         default=0
                         )
 
-    args = parser.parse_args()
-    res = run(MACSY_HOME, args.tests, args.verbosity)
+    args = parser.parse_args(args)
+
+    MACSY_HOME = os.path.abspath(os.path.join(__file__, '..', '..'))
+
+    old_path = sys.path
+
+    if MACSY_HOME not in sys.path:
+        # need to add tests in path
+        # tests inherits from IntegronTest which is located in tests/__init__.py
+        sys.path.insert(0, MACSY_HOME)
+
+    test_runner = run_tests(args.tests, verbosity=args.verbosity)
+    unit_results = test_runner.wasSuccessful()
+    sys.path = old_path
+    return unit_results
+
+
+if __name__ == '__main__':
+
+    unit_results = main(sys.argv[1:])
+    if unit_results:
+        sys.exit(0)
+    else:
+        sys.exit(1)
