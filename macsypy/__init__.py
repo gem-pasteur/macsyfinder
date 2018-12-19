f7aacfc499b3ea455e4ae27089916c24b216030b
Bertrand <bneron@pasteur.fr>
2018-12-19 15:55:35 +0100

    :art: fix vesion to be closed to PEP440

--- macsypy/__init__.py
+++ macsypy/__init__.py
@@ -1,6 +1,6 @@
 from time import strftime, localtime
 
-__version__ = 'master-{}'.format(strftime("%Y%m%d", localtime()))
+__version__ = '{}.dev'.format(strftime("%Y%m%d", localtime()))
 
 __MACSY_CONF__ = '$MACSYCONF'
 __MACSY_DATA__ = '$MACSYDATA'
