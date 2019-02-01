86f5188b22638d3d5103407a1ce671c0e879ae73
Bertrand Néron <bneron@pasteur.fr>
2019-02-01 11:42:05 +0100

    :bug: fix test according to macsy_test_env atribute models signature

--- tests/test_validSystemHit.py
+++ tests/test_validSystemHit.py
@@ -21,30 +21,27 @@ class Test(MacsyTest, MacsyEnvManager):
 
     def setUp(self):
         self.load_env("env_003", log_out=False,)
+        self.model = [m for m in self.macsy_test_env.models if m.name == 'T9SS'][0]
+        self.hit = [h for h in self.macsy_test_env.all_hits if h.gene.name == 'T9SS_sprT'][0]
 
     def tearDown(self):
         self.unload_env("env_003")
-        pass
 
     def test_str(self):
-        hit = self.macsy_test_env.all_hits[0]
-        valid_system_hit = validSystemHit(hit, self.macsy_test_env.model, "mandatory")
+        valid_system_hit = validSystemHit(self.hit, self.model, "mandatory")
         self.assertEqual(str(valid_system_hit), self.output_control_str('001'))
 
     def test_output_system(self):
-        hit = self.macsy_test_env.all_hits[0]
-        valid_system_hit = validSystemHit(hit, self.macsy_test_env.model, "mandatory")
+        valid_system_hit = validSystemHit(self.hit, self.model, "mandatory")
         report_str = valid_system_hit.output_system("foo132", "all_clear")
         self.assertEqual(str(report_str), self.output_control_str('001'))
 
     def test_getattr(self):
-        hit = self.macsy_test_env.all_hits[0]
-        valid_system_hit = validSystemHit(hit, self.macsy_test_env.model, "mandatory")
+        valid_system_hit = validSystemHit(self.hit, self.model, "mandatory")
         i_eval = valid_system_hit.i_eval
         self.assertEqual(i_eval, 7.5e-103)
 
     def test_output_system_header(self):
-        hit = self.macsy_test_env.all_hits[0]
-        valid_system_hit = validSystemHit(hit, self.macsy_test_env.model, "mandatory")
+        valid_system_hit = validSystemHit(self.hit, self.model, "mandatory")
         header_str = valid_system_hit.output_system_header()
         self.assertEqual(header_str, self.output_control_str('001'))
