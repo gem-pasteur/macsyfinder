28090cc420255806aaaef1f024166a5e939d2f1b
Bertrand <bneron@pasteur.fr>
2019-02-01 10:55:12 +0100

    :art: mv get_models_name_to_detect into utils module

--- macsypy/scripts/macsyfinder.py
+++ macsypy/scripts/macsyfinder.py
@@ -32,34 +32,7 @@ from macsypy.search_systems import search_systems
 from macsypy.model import model_bank
 from macsypy.gene import gene_bank
 from macsypy.error import OptionError
-
-
-
-def get_models_name_to_detect(models, model_registry):
-    """
-    :param models: the list of models to detect as returned by config.models.
-    :type models: list of tuple with the following structure:
-                  [('model_1', ('def1, def2, ...)), ('model_2', ('def1', ...)), ...]
-    :param model_registry: the models registry for this run.
-    :type model_registry: :class:`macsypy.registries.ModelRegistry` object.
-    :return: the models fully qualified name to launch a detection on.
-    :rtype: list of string ['model_1/def1', 'model_1/def2', ..., 'model_2/def1', ...]
-    :raise ValueError: if a model name provided in models is not in model_registry.
-    """
-    models_name_to_detect = []
-    for group_of_defs in models:
-        root = group_of_defs[0]
-        definitions = group_of_defs[1]
-        model_loc = model_registry[root.split('/')[0]]
-        if 'all' in [d.lower() for d in definitions]:
-            if root == model_loc.name:
-                root = None
-            def_loc = model_loc.get_all_definitions(root_def_name=root)
-            models_name_to_detect.extend([d.fqn for d in def_loc])
-        else:
-            models_name_to_detect.extend([model_loc.get_definition('{}/{}'.format(root, one_def)).fqn
-                                          for one_def in definitions])
-    return models_name_to_detect
+from macsypy.utils import get_models_name_to_detect
 
 
 def get_version_message():
