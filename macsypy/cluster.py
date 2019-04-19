f7913fcf4a007cbe49e66c1ed87fcfd73691edb1
Bertrand Néron <bneron@pasteur.fr>
2019-04-19 16:58:50 +0200

    :art: add first implementaion of build cluster
    do not handle loner andmulti system

--- macsypy/cluster.py
+++ macsypy/cluster.py
@@ -1,4 +1,9 @@
 import itertools
+import logging
+
+from .error import MacsypyError
+
+_log = logging.getLogger(__name__)
 
 
 def build_clusters(hits, rep_info, model):
@@ -21,66 +26,45 @@ def build_clusters(hits, rep_info, model):
     """
     def is_in_cluster(h1, h2):
         dist = h2.get_position() - h1.get_position() - 1
-        if 0 < dist <= model.max_gene_inter_space:
+        if 0 < dist <= model.inter_gene_max_space:
             return True
         elif dist < 0 and rep_info.topology == 'circular':
-            # h1 et h2 sont par et d'autres ori
-            dist = rep_info.length - h2.get_position + h1.get_position()
-            return dist <= model.max_gene_inter_space
+            # h1 and h2 overlap the ori
+            dist = rep_info.max - h1.get_position() + h2.get_position() - rep_info.min
+            return dist <= model.inter_gene_max_space
         return False
 
-
     clusters = []
-    cluster_building = []
-    # trier les hits par position croissantes
-    hits.sort(key=lambda hit: hit.position_hit)
-    # enlever les duplicats (meme sequence)
+    cluster_scaffold = []
+    # sort hits by increasing position and then descending score
+    hits.sort(key=lambda h: (h.position, - h.score))
+    # remove duplicates hits (several hits for the same sequence),
+    # keep the first one, this with the best score
+    # position == sequence rank in replicon
     hits = ([next(group) for pos, group in itertools.groupby(hits, lambda h: h.position)])
-
-    cluster_building.append(hits[0])
+    cluster_scaffold.append(hits[0])
     previous_hit = hits[0]
-    for hit in hits:
+    for hit in hits[1:]:
         if is_in_cluster(previous_hit, hit):
-            cluster_building.append(hit)
+            cluster_scaffold.append(hit)
         else:
-            if len(cluster_building) > 1:
-                cluster = Cluster(cluster_building, model)
+            if len(cluster_scaffold) > 1:
+                cluster = Cluster(cluster_scaffold, model)
                 clusters.append(cluster)
-            cluster_building = []
+            cluster_scaffold = [hit]
         previous_hit = hit
 
-    if len(cluster_building) > 1:
-        # creer un cluster
-        # si dernier hit du dernier cluster et 1er hit du 1er cluster < is_in_cluster
-        #   fusionner les 2 clusters  dernier + 1er
-        #sinon ajouter dernier cluster a la liste
+    if len(cluster_scaffold) > 1:
+        new_cluster = Cluster(cluster_scaffold, model)
+        if is_in_cluster(new_cluster.hits[-1], clusters[0].hits[0]):
+            clusters[0].merge(new_cluster, before=True)
+        else:
+            clusters.append(new_cluster)
     else:
-        # regarder si previous hit et 1er hit du 1er cluster < is_in_cluster
-        # ajouter hit au 1er cluster
+        if is_in_cluster(previous_hit, clusters[0].hits[0]):
+            clusters[0].merge(Cluster([previous_hit], model), before=True)
 
-
-
-    # ajouter le 1er elt de la liste dans une liste potentielle
-    # calculer la distance entre 2 hits (en temr de nbre de genes)
-    # si cette distance <= max_gene_inter_space
-    #   ajouter le hit dans la liste du cluster
-    # si non arreter la liste
-    #    si la liste < 2 jeter la liste
-
-    # cas des loner
-    # cas des replicons circulaire
-    #   => fusion du 1er et dernier cluster
-    #  extension des derniers hits avec les premiers
-
-    # replicon or RepliconInfo
-    # Replicon info contains
-    #  topology
-    #  _min: rank of the first sequence in file
-    #  _max: rank of the first sequence in file
-    #  genes: (seq_name, seq_len)
-    # taille du replicon ?
-    #    en nombre de genes OK
-    #    en nombre d'aa ??
+    return clusters
 
 
 class Cluster:
@@ -89,17 +73,37 @@ class Cluster:
         self.hits = hits
         self.model = model
 
+    def merge(self, cluster, before=False):
+        """
+        merge the cluster in this one.
 
-    def match(self, model):
+        :param cluster:
+        :type cluster:
+        :param bool before: If True the hits of the cluster will be add at the end of this one,
+                            Otherwise the cluster hits be insert before the hits of this one
+        :return: None
+        :raise MasypyError: if the two cluster have not the same model
         """
-        test if this cluster match a model
-        matching a model means satisfy the quorum constraints:
-          * number of mandatory,
-          * accessory,
-          * presence of forbidden
-        :param model: the model to chalenge
-        :type model: :class:`macsypy.model.Model` object
-        :return: a Putative System
-        :rtype: :class:`Putative System` object
-        """
-        return True
+        if cluster.model != self.model:
+            raise MacsypyError("Try to merge Clusters from different model")
+        else:
+            if before:
+                self.hits = cluster.hits + self.hits
+            else:
+                self.hits.extend(cluster.hits)
+
+
+    def __str__(self):
+        s = """Cluster:
+    - model: {}
+    - hits: {}""".format(self.model.name, ', '.join(["({}, {})".format(h.id,
+                                                                       h.gene.name,
+                                                                       h.position) for h in self.hits]))
+        return s
+
+
+class RejectedCluster:
+
+    def __init__(self, cluster, reason):
+        self.cluster = cluster
+        self.reason = reason
