cd0217ebd115a62fec98d1b9ea2c7facaa56b473
Bertrand <bneron@pasteur.fr>
2019-04-25 17:36:25 +0200

    :white_check_mark: add unit tests for clusters and clusters building

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -10,12 +10,10 @@
 # MacsyFinder is distributed under the terms of the GNU General Public License #
 # (GPLv3). See the COPYING file for details.                                   #
 ################################################################################
-import os
 import argparse
-import tempfile
-import shutil
 import random
 
+from macsypy.error import MacsypyError
 from macsypy.config import Config, MacsyDefaults
 from macsypy.registries import ModelRegistry
 from macsypy.gene import Gene, ProfileFactory
@@ -33,25 +31,13 @@ class TestCluster(MacsyTest):
         self.args.sequence_db = self.find_data("base", "test_base.fa")
         self.args.db_type = 'gembase'
         self.args.models_dir = self.find_data('models')
-        self.args.res_search_dir = tempfile.gettempdir()
-        self.args.log_level = 30
-        self.args.out_dir = os.path.join(self.args.res_search_dir, 'test_macsyfinder_cluster')
-        if os.path.exists(self.args.out_dir):
-            shutil.rmtree(self.args.out_dir)
-        os.mkdir(self.args.out_dir)
+        self.args.res_search_dir = "blabla"
 
         self.cfg = Config(MacsyDefaults(), self.args)
         models_registry = ModelRegistry(self.cfg)
         self.model_name = 'foo'
         self.models_location = models_registry[self.model_name]
         self.profile_factory = ProfileFactory()
-
-
-    def tearDown(self):
-        try:
-            shutil.rmtree(self.cfg.working_dir())
-        except:
-            pass
 
 
     def test_build_clusters(self):
@@ -64,6 +50,7 @@ class TestCluster(MacsyTest):
         gene_2 = Gene(self.cfg, self.profile_factory, "sctC", model, self.models_location)
         gene_3 = Gene(self.cfg, self.profile_factory, "sctJ", model, self.models_location)
         gene_4 = Gene(self.cfg, self.profile_factory, "sctN", model, self.models_location)
+        gene_5 = Gene(self.cfg, self.profile_factory, "abc", model, self.models_location, loner=True)
 
         #     Hit(gene, model, hit_id, hit_seq_length, replicon_name, position, i_eval, score,
         #         profile_coverage, sequence_coverage, begin_match, end_match
@@ -77,17 +64,39 @@ class TestCluster(MacsyTest):
         h51 = Hit(gene_3, model, "h51", 10, "replicon_1", 50, 1.0, 51.0, 1.0, 1.0, 10, 20)
         h60 = Hit(gene_3, model, "h60", 10, "replicon_1", 60, 1.0, 60.0, 1.0, 1.0, 10, 20)
         h61 = Hit(gene_4, model, "h61", 10, "replicon_1", 60, 1.0, 61.0, 1.0, 1.0, 10, 20)
+
+        # case replicon is linear, 2 clusters
         hits = [h10, h11, h20, h21, h30, h31, h50, h51, h60, h61]
         random.shuffle(hits)
-
-        # case replicon is linear , 2 clusters
         clusters = build_clusters(hits, rep_info, model)
         self.assertEqual(len(clusters), 2)
         self.assertListEqual(clusters[0].hits, [h11, h21, h31])
         self.assertListEqual(clusters[1].hits, [h51, h61])
 
+        # case replicon is linear with a single hit (not loner) between 2 clusters
+        h70 = Hit(gene_4, model, "h70", 10, "replicon_1", 70, 1.0, 80.0, 1.0, 1.0, 10, 20)
+        h80 = Hit(gene_5, model, "h80", 10, "replicon_1", 80, 1.0, 80.0, 1.0, 1.0, 10, 20)
+        hits = [h10, h11, h20, h21, h50, h51, h70, h80]
+        random.shuffle(hits)
+        clusters = build_clusters(hits, rep_info, model)
+        self.assertEqual(len(clusters), 2)
+        self.assertListEqual(clusters[0].hits, [h11, h21])
+        self.assertListEqual(clusters[1].hits, [h70, h80])
+
+        # replicon is linear, 3 clusters, the last one contains only one hit (loner)
+        rep_info = RepliconInfo('linear', 1, 100, [("g_{}".format(i), i*10) for i in range(1, 101)])
+        h80 = Hit(gene_5, model, "h80", 10, "replicon_1", 80, 1.0, 80.0, 1.0, 1.0, 10, 20)
+        hits = [h10, h11, h20, h21, h30, h31, h50, h51, h60, h61, h80]
+        random.shuffle(hits)
+        clusters = build_clusters(hits, rep_info, model)
+        self.assertEqual(len(clusters), 3)
+        self.assertListEqual(clusters[0].hits, [h11, h21, h31])
+        self.assertListEqual(clusters[1].hits, [h51, h61])
+        self.assertListEqual(clusters[2].hits, [h80])
+
         # replicon is circular the last cluster is merge  with the first So we have only one cluster
         rep_info = RepliconInfo('circular', 1, 60, [("g_{}".format(i), i*10) for i in range(1, 7)])
+        hits = [h10, h11, h20, h21, h30, h31, h50, h51, h60, h61]
         clusters = build_clusters(hits, rep_info, model)
         self.assertEqual(len(clusters), 1)
         self.assertListEqual(clusters[0].hits, [h51, h61, h11, h21, h31])
@@ -95,7 +104,7 @@ class TestCluster(MacsyTest):
         # relicon is circular the last hit is incorporate to the first cluster
         rep_info = RepliconInfo('circular', 1, 80, [("g_{}".format(i), i*10) for i in range(1, 9)])
         h80 = Hit(gene_4, model, "h80", 10, "replicon_1", 80, 1.0, 80.0, 1.0, 1.0, 10, 20)
-        hits.append(h80)
+        hits = [h10, h11, h20, h21, h30, h31, h50, h51, h60, h61, h80]
         random.shuffle(hits)
         clusters = build_clusters(hits, rep_info, model)
         self.assertEqual(len(clusters), 2)
@@ -104,12 +113,32 @@ class TestCluster(MacsyTest):
 
         # replicon is circular the last hit is not merged with the first cluster
         rep_info = RepliconInfo('linear', 1, 80, [("g_{}".format(i), i*10) for i in range(1, 9)])
+        hits = [h10, h11, h20, h21, h30, h31, h50, h51, h60, h61, h80]
+        random.shuffle(hits)
         clusters = build_clusters(hits, rep_info, model)
         self.assertEqual(len(clusters), 2)
         self.assertEqual(len(clusters), 2)
         self.assertListEqual(clusters[0].hits, [h11, h21, h31])
         self.assertListEqual(clusters[1].hits, [h51, h61])
 
+    def test_init(self):
+        model_1 = Model(self.cfg, "foo/T2SS", 11)
+        model_2 = Model(self.cfg, "foo/T3SS", 11)
+
+        gene_1 = Gene(self.cfg, self.profile_factory, "gspD", model_1, self.models_location)
+        gene_2 = Gene(self.cfg, self.profile_factory, "sctC", model_1, self.models_location)
+        gene_3 = Gene(self.cfg, self.profile_factory, "sctJ", model_2, self.models_location)
+
+        h10 = Hit(gene_1, model_1, "h10", 10, "replicon_1", 10, 1.0, 10.0, 1.0, 1.0, 10, 20)
+        h20 = Hit(gene_2, model_1, "h20", 10, "replicon_1", 20, 1.0, 20.0, 1.0, 1.0, 10, 20)
+        h30 = Hit(gene_3, model_2, "h30", 10, "replicon_2", 30, 1.0, 30.0, 1.0, 1.0, 10, 20)
+        h50 = Hit(gene_3, model_2, "h50", 10, "replicon_2", 50, 1.0, 50.0, 1.0, 1.0, 10, 20)
+
+        with self.assertRaises(MacsypyError) as ctx:
+            with self.catch_log():
+                Cluster([h10, h20, h30, h50], model_1)
+        msg = "Cannot build a cluster from hits coming from different replicons"
+        self.assertEqual(str(ctx.exception), msg)
 
     def test_merge(self):
         model = Model(self.cfg, "foo/T2SS", 11)
@@ -139,6 +168,16 @@ class TestCluster(MacsyTest):
         c2 = Cluster([h30, h50], model)
         c1.merge(c2, before=True)
         self.assertListEqual(c1.hits, [h30, h50, h10, h20])
+
+        model_2 = Model(self.cfg, "foo/T3SS", 11)
+        gene_3 = Gene(self.cfg, self.profile_factory, "sctJ", model_2, self.models_location)
+        h30 = Hit(gene_3, model_2, "h30", 10, "replicon_2", 30, 1.0, 30.0, 1.0, 1.0, 10, 20)
+        h50 = Hit(gene_3, model_2, "h50", 10, "replicon_2", 50, 1.0, 50.0, 1.0, 1.0, 10, 20)
+        c3 = Cluster([h30, h50], model_2)
+        with self.assertRaises(MacsypyError) as ctx:
+            c1.merge(c3)
+        self.assertEqual(str(ctx.exception), "Try to merge Clusters from different model")
+
 
     def test_str(self):
         model = Model(self.cfg, "foo/T2SS", 11)
