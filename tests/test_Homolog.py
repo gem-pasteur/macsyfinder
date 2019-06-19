453a2b094d8477c509a0fd5b227ee86ae5adb11e
Bertrand <bneron@pasteur.fr>
2019-06-19 11:45:58 +0200

    :art: update header
    update copyright date
    remove unicode characters
    ...

--- tests/test_Homolog.py
+++ tests/test_Homolog.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-# MacSyFinder - Detection of macromolecular models in protein datasets        #
-#               using models modelling and similarity search.                 #
-# Authors: Sophie Abby, Bertrand Néron                                         #
-# Copyright © 2014  Institut Pasteur, Paris.                                   #
+# MacSyFinder - Detection of macromolecular systems in protein datasets        #
+#               using systems modelling and similarity search.                 #
+# Authors: Sophie Abby, Bertrand Neron                                         #
+# Copyright (c) 2014-2019  Institut Pasteur (Paris) and CNRS.                  #
 # See the COPYRIGHT file for details                                           #
 #                                                                              #
 # MacsyFinder is distributed under the terms of the GNU General Public License #
