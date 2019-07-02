4359149c5f0095b2bb729006e9ea1aaf36f5f93e
Bertrand <bneron@pasteur.fr>
2019-07-02 11:15:11 +0200

    :art: delete utils which function
    as the same code is also present in tests/__init__.py
    as which is used only in tests used the tests/__init__ version
    and fix imports

---
+++ tests/test_utils.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+# MacSyFinder - Detection of macromolecular systems in protein datasets        #
+#               using systems modelling and similarity search.                 #
+# Authors: Sophie Abby, Bertrand Neron                                         #
+# Copyright (c) 2014-2019  Institut Pasteur (Paris) and CNRS.                  #
+# See the COPYRIGHT file for details                                           #
+#                                                                              #
+# MacsyFinder is distributed under the terms of the GNU General Public License #
+# (GPLv3). See the COPYING file for details.                                   #
+################################################################################
+
+import os
+import shutil
+import tempfile
+import argparse
+import glob
+
+from macsypy.config import Config, MacsyDefaults
+from macsypy.registries import ModelRegistry
+from macsypy.utils import get_models_name_to_detect
+from tests import MacsyTest
+
+
+class TestUtils(MacsyTest):
+
+    def setUp(self):
+        self.tmp_dir = tempfile.mkdtemp()
+
+
+    def tearDown(self):
+        try:
+            shutil.rmtree(self.tmp_dir)
+        except:
+            pass
+
+    def test_models_name_to_detect(self):
+        cmd_args = argparse.Namespace()
+        cmd_args.models_dir = os.path.join(self._data_dir, 'data_set_1', 'models')
+        cmd_args.models = [('set_1', 'T9SS', 'T3SS', 'T4SS_typeI')]
+        config = Config(MacsyDefaults(models_dir=os.path.join(self._data_dir, 'data_set_1', 'models')),
+                        cmd_args)
+        registry = ModelRegistry(config)
+        # case where models are specified on command line
+        res = get_models_name_to_detect([('set_1', ['T9SS', 'T3SS', 'T4SS_typeI'])], registry)
+        exp = ['set_1/T9SS', 'set_1/T3SS', 'set_1/T4SS_typeI']
+        self.assertListEqual(res, exp)
+
+        # case we search all models
+        res = get_models_name_to_detect([('set_1', ['all'])], registry)
+        exp = ["set_1/{}".format(os.path.splitext(os.path.basename(name))[0]) for name in
+               glob.glob(os.path.join(config.models_dir(), 'set_1', 'definitions', '*.xml'))]
+        self.assertListEqual(res, exp)
+
+        # case the models required does not exists
+        with self.assertRaises(ValueError):
+            get_models_name_to_detect([('set_1', ['FOO', 'BAR'])], registry)
+
+
