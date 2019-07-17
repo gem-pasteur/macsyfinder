37da9cc202159801209fbdb49ec69a530c0a7c68
Bertrand <bneron@pasteur.fr>
2019-07-17 15:31:51 +0200

    :white_check_mark: add tests to new method has and score

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -17,8 +17,8 @@ import random
 from macsypy.error import MacsypyError
 from macsypy.config import Config, MacsyDefaults
 from macsypy.registries import ModelRegistry
-from macsypy.gene import Gene, ProfileFactory
-from macsypy.hit import Hit
+from macsypy.gene import Gene, Homolog, Analog, ProfileFactory, GeneStatus
+from macsypy.hit import Hit, ValidHit
 from macsypy.model import Model
 from macsypy.database import RepliconInfo
 from macsypy.cluster import Cluster, build_clusters, RejectedClusters, get_loners, filter_loners
@@ -268,13 +268,17 @@ class TestCluster(MacsyTest):
         gene_3 = Gene(self.cfg, self.profile_factory, "sctJ", model_2, self.models_location)
 
         h10 = Hit(gene_1, model_1, "h10", 10, "replicon_1", 10, 1.0, 10.0, 1.0, 1.0, 10, 20)
+        v_h10 = ValidHit(h10, gene_1, GeneStatus.MANDATORY)
         h20 = Hit(gene_2, model_1, "h20", 10, "replicon_1", 20, 1.0, 20.0, 1.0, 1.0, 10, 20)
+        v_h20 = ValidHit(h20, gene_1, GeneStatus.MANDATORY)
         h30 = Hit(gene_3, model_2, "h30", 10, "replicon_2", 30, 1.0, 30.0, 1.0, 1.0, 10, 20)
+        v_h30 = ValidHit(h30, gene_1, GeneStatus.ACCESSORY)
         h50 = Hit(gene_3, model_2, "h50", 10, "replicon_2", 50, 1.0, 50.0, 1.0, 1.0, 10, 20)
+        v_h50 = ValidHit(h50, gene_1, GeneStatus.ACCESSORY)
 
         with self.assertRaises(MacsypyError) as ctx:
             with self.catch_log():
-                Cluster([h10, h20, h30, h50], model_1)
+                Cluster([v_h10, v_h20, v_h30, v_h50], model_1)
         msg = "Cannot build a cluster from hits coming from different replicons"
         self.assertEqual(str(ctx.exception), msg)
 
@@ -288,10 +292,15 @@ class TestCluster(MacsyTest):
         #     Hit(gene, model, hit_id, hit_seq_length, replicon_name, position, i_eval, score,
         #         profile_coverage, sequence_coverage, begin_match, end_match
         h10 = Hit(gene_1, model, "h10", 10, "replicon_1", 10, 1.0, 10.0, 1.0, 1.0, 10, 20)
+        v_h10 = ValidHit(h10, gene_1, GeneStatus.MANDATORY)
         h20 = Hit(gene_2, model, "h20", 10, "replicon_1", 20, 1.0, 20.0, 1.0, 1.0, 10, 20)
+        v_h20 = ValidHit(h20, gene_2, GeneStatus.MANDATORY)
         h30 = Hit(gene_3, model, "h30", 10, "replicon_1", 30, 1.0, 30.0, 1.0, 1.0, 10, 20)
+        v_h30 = ValidHit(h30, gene_3, GeneStatus.ACCESSORY)
         h50 = Hit(gene_3, model, "h50", 10, "replicon_1", 50, 1.0, 50.0, 1.0, 1.0, 10, 20)
-        c1 = Cluster([h10, h20], model)
+        v_h50 = ValidHit(h50, gene_3, GeneStatus.ACCESSORY)
+
+        c1 = Cluster([v_h10, v_h20], model)
         self.assertEqual(len(c1), 2)
         
 
@@ -305,13 +314,111 @@ class TestCluster(MacsyTest):
         #     Hit(gene, model, hit_id, hit_seq_length, replicon_name, position, i_eval, score,
         #         profile_coverage, sequence_coverage, begin_match, end_match
         h10 = Hit(gene_1, model, "h10", 10, "replicon_1", 10, 1.0, 10.0, 1.0, 1.0, 10, 20)
+        v_h10 = ValidHit(h10, gene_1, GeneStatus.MANDATORY)
         h20 = Hit(gene_2, model, "h20", 10, "replicon_1", 20, 1.0, 20.0, 1.0, 1.0, 10, 20)
+        v_h20 = ValidHit(h20, gene_2, GeneStatus.MANDATORY)
         h30 = Hit(gene_3, model, "h30", 10, "replicon_1", 30, 1.0, 30.0, 1.0, 1.0, 10, 20)
+        v_h30 = ValidHit(h30, gene_3, GeneStatus.ACCESSORY)
         h50 = Hit(gene_3, model, "h50", 10, "replicon_1", 50, 1.0, 50.0, 1.0, 1.0, 10, 20)
-        c1 = Cluster([h10, h20], model)
+        v_h50 = ValidHit(h50, gene_3, GeneStatus.ACCESSORY)
+        c1 = Cluster([v_h10, v_h20, v_h50], model)
 
-        self.assertTrue(h10 in c1)
-        self.assertFalse(h30 in c1)
+        self.assertTrue(v_h10 in c1)
+        self.assertFalse(v_h30 in c1)
+
+
+    def test_has(self):
+        model = Model(self.cfg, "foo/T2SS", 11)
+
+        gene_1 = Gene(self.cfg, self.profile_factory, "gspD", model, self.models_location)
+        gene_2 = Gene(self.cfg, self.profile_factory, "sctC", model, self.models_location)
+        gene_3 = Gene(self.cfg, self.profile_factory, "sctJ", model, self.models_location)
+
+        #     Hit(gene, model, hit_id, hit_seq_length, replicon_name, position, i_eval, score,
+        #         profile_coverage, sequence_coverage, begin_match, end_match
+        h10 = Hit(gene_1, model, "h10", 10, "replicon_1", 10, 1.0, 10.0, 1.0, 1.0, 10, 20)
+        v_h10 = ValidHit(h10, gene_1, GeneStatus.MANDATORY)
+        h20 = Hit(gene_2, model, "h20", 10, "replicon_1", 20, 1.0, 20.0, 1.0, 1.0, 10, 20)
+        v_h20 = ValidHit(h20, gene_2, GeneStatus.MANDATORY)
+        h30 = Hit(gene_3, model, "h30", 10, "replicon_1", 30, 1.0, 30.0, 1.0, 1.0, 10, 20)
+        v_h30 = ValidHit(h30, gene_3, GeneStatus.ACCESSORY)
+        h50 = Hit(gene_3, model, "h50", 10, "replicon_1", 50, 1.0, 50.0, 1.0, 1.0, 10, 20)
+        v_h50 = ValidHit(h50, gene_2, GeneStatus.ACCESSORY)
+        c1 = Cluster([v_h10, v_h20], model)
+
+        self.assertTrue(c1.has(v_h10))
+        self.assertFalse(c1.has(v_h30))
+        self.assertTrue(c1.has(v_h50))
+
+    def test_score(self):
+        model = Model(self.cfg, "foo/T2SS", 10)
+        gene_gspd = Gene(self.cfg, self.profile_factory, "gspD", model, self.models_location)
+        model.add_mandatory_gene(gene_gspd)
+        gene_tadZ = Gene(self.cfg, self.profile_factory, "tadZ", model, self.models_location)
+        model.add_mandatory_gene(gene_tadZ)
+
+        gene_sctj = Gene(self.cfg, self.profile_factory, "sctJ", model, self.models_location, exchangeable=True)
+        gene_sctJ_FLG = Gene(self.cfg, self.profile_factory, 'sctJ_FLG', model, self.models_location)
+        analog = Analog(gene_sctJ_FLG, gene_sctj)
+        gene_sctj.add_analog(analog)
+        model.add_accessory_gene(gene_sctj)
+
+        gene_sctn = Gene(self.cfg, self.profile_factory, "sctN", model, self.models_location, loner=True)
+        gene_sctn_FLG = Gene(self.cfg, self.profile_factory, 'sctN_FLG', model, self.models_location)
+        homolog = Homolog(gene_sctn_FLG, gene_sctj)
+        gene_sctn.add_homolog(homolog)
+        model.add_accessory_gene(gene_sctn)
+
+        h_gspd = Hit(gene_gspd, model, "h_gspd", 10, "replicon_id", 1, 1.0, 1.0, 1.0, 1.0, 10, 20)
+        v_h_gspd = ValidHit(h_gspd, gene_gspd, GeneStatus.MANDATORY)
+        h_tadz = Hit(gene_tadZ, model, "h_tadz", 20, "replicon_id", 1, 1.0, 1.0, 1.0, 1.0, 10, 20)
+        v_h_tadz = ValidHit(h_tadz, gene_tadZ, GeneStatus.MANDATORY)
+
+        h_sctj = Hit(gene_sctj, model, "h_sctj", 30, "replicon_id", 1, 1.0, 1.0, 1.0, 1.0, 10, 20)
+        v_h_sctj = ValidHit(h_sctj, gene_sctj, GeneStatus.ACCESSORY)
+        h_sctj_an = Hit(gene_sctJ_FLG, model, "h_sctj_an", 30, "replicon_id", 1, 1.0, 1.0, 1.0, 1.0, 10, 20)
+        v_h_sctj_an = ValidHit(h_sctj_an, gene_sctj, GeneStatus.ACCESSORY)
+
+        h_sctn = Hit(gene_sctn, model, "sctn", 40, "replicon_id", 1, 1.0, 1.0, 1.0, 1.0, 10, 20)
+        v_h_sctn = ValidHit(h_sctn, gene_sctn, GeneStatus.ACCESSORY)
+        h_sctn_hom = Hit(gene_sctn_FLG, model, "h_scth_hom", 30, "replicon_id", 1, 1.0, 1.0, 1.0, 1.0, 10, 20)
+        v_h_sctn_hom = ValidHit(h_sctn_hom, gene_sctn, GeneStatus.ACCESSORY)
+
+        # 2 mandatory, 2 accessory no analog/homolog
+        c1 = Cluster([v_h_gspd, v_h_tadz, v_h_sctj, v_h_sctn], model)
+        self.assertEqual(c1.score, 3.0)
+
+        # 1 mandatory + 1 mandatory duplicated 1 time
+        # 1 accessory + 1 accessory duplicated 1 times
+        # no analog/homolog
+        c1 = Cluster([v_h_gspd, v_h_tadz, v_h_sctj, v_h_sctn, v_h_gspd, v_h_sctn], model)
+        self.assertEqual(c1.score, 3.0)
+
+        # 2 mandatory
+        # 1 accessory + 1 accessory homolog
+        c1 = Cluster([v_h_gspd, v_h_tadz, v_h_sctj, v_h_sctn_hom], model)
+        self.assertEqual(c1.score, 2.875)
+
+        # 2 mandatory
+        # 1 accessory + 1 accessory analog
+        c1 = Cluster([v_h_gspd, v_h_tadz, v_h_sctn, v_h_sctj_an], model)
+        self.assertEqual(c1.score, 2.875)
+
+        # 2 mandatory
+        # 1 accessory + 1 accessory analog of the 1rst accessory
+        c1 = Cluster([v_h_gspd, v_h_tadz, v_h_sctj, v_h_sctj_an], model)
+        self.assertEqual(c1.score, 2.5)
+
+        # test the cache score
+        self.assertEqual(c1.score, 2.5)
+
+        non_valid_hit = ValidHit(h_sctn, gene_sctn, GeneStatus.FORBIDDEN)
+        c1 = Cluster([v_h_gspd, non_valid_hit, v_h_tadz], model)
+        with self.assertRaises(MacsypyError) as ctx:
+            c1.score
+        self.assertEqual(str(ctx.exception),
+                         "a Cluster contains hit which is neither mandatory nor accessory")
+
 
 
     def test_merge(self):
@@ -324,30 +431,36 @@ class TestCluster(MacsyTest):
         #     Hit(gene, model, hit_id, hit_seq_length, replicon_name, position, i_eval, score,
         #         profile_coverage, sequence_coverage, begin_match, end_match
         h10 = Hit(gene_1, model, "h10", 10, "replicon_1", 10, 1.0, 10.0, 1.0, 1.0, 10, 20)
+        v_h10 = ValidHit(h10, gene_1, GeneStatus.MANDATORY)
         h20 = Hit(gene_2, model, "h20", 10, "replicon_1", 20, 1.0, 20.0, 1.0, 1.0, 10, 20)
+        v_h20 = ValidHit(h20, gene_2, GeneStatus.MANDATORY)
         h30 = Hit(gene_3, model, "h30", 10, "replicon_1", 30, 1.0, 30.0, 1.0, 1.0, 10, 20)
+        v_h30 = ValidHit(h30, gene_3, GeneStatus.ACCESSORY)
         h50 = Hit(gene_3, model, "h50", 10, "replicon_1", 50, 1.0, 50.0, 1.0, 1.0, 10, 20)
+        v_h50 = ValidHit(h50, gene_3, GeneStatus.ACCESSORY)
 
-        c1 = Cluster([h10, h20], model)
-        c2 = Cluster([h30, h50], model)
+        c1 = Cluster([v_h10, v_h20], model)
+        c2 = Cluster([v_h30, v_h50], model)
         c1.merge(c2)
-        self.assertListEqual(c1.hits, [h10, h20, h30, h50])
+        self.assertListEqual(c1.hits, [v_h10, v_h20, v_h30, v_h50])
 
-        c1 = Cluster([h10, h20], model)
-        c2 = Cluster([h30, h50], model)
+        c1 = Cluster([v_h10, v_h20], model)
+        c2 = Cluster([v_h30, v_h50], model)
         c2.merge(c1)
-        self.assertListEqual(c2.hits, [h30, h50, h10, h20])
+        self.assertListEqual(c2.hits, [v_h30, v_h50, v_h10, v_h20])
 
-        c1 = Cluster([h10, h20], model)
-        c2 = Cluster([h30, h50], model)
+        c1 = Cluster([v_h10, v_h20], model)
+        c2 = Cluster([v_h30, v_h50], model)
         c1.merge(c2, before=True)
-        self.assertListEqual(c1.hits, [h30, h50, h10, h20])
+        self.assertListEqual(c1.hits, [v_h30, v_h50, v_h10, v_h20])
 
         model_2 = Model(self.cfg, "foo/T3SS", 11)
         gene_3 = Gene(self.cfg, self.profile_factory, "sctJ", model_2, self.models_location)
         h30 = Hit(gene_3, model_2, "h30", 10, "replicon_2", 30, 1.0, 30.0, 1.0, 1.0, 10, 20)
+        v_h30 = ValidHit(h30, gene_3, GeneStatus.ACCESSORY)
         h50 = Hit(gene_3, model_2, "h50", 10, "replicon_2", 50, 1.0, 50.0, 1.0, 1.0, 10, 20)
-        c3 = Cluster([h30, h50], model_2)
+        v_h50 = ValidHit(h50, gene_3, GeneStatus.ACCESSORY)
+        c3 = Cluster([v_h30, v_h50], model_2)
         with self.assertRaises(MacsypyError) as ctx:
             c1.merge(c3)
         self.assertEqual(str(ctx.exception), "Try to merge Clusters from different model")
@@ -362,8 +475,10 @@ class TestCluster(MacsyTest):
         #     Hit(gene, model, hit_id, hit_seq_length, replicon_name, position, i_eval, score,
         #         profile_coverage, sequence_coverage, begin_match, end_match
         h10 = Hit(gene_1, model, "h10", 10, "replicon_1", 10, 1.0, 10.0, 1.0, 1.0, 10, 20)
+        v_h10 = ValidHit(h10, gene_1, GeneStatus.MANDATORY)
         h20 = Hit(gene_2, model, "h20", 10, "replicon_1", 20, 1.0, 20.0, 1.0, 1.0, 10, 20)
-        c1 = Cluster([h10, h20], model)
+        v_h20 = ValidHit(h20, gene_2, GeneStatus.MANDATORY)
+        c1 = Cluster([v_h10, v_h20], model)
         s ="""Cluster:
     - model: T2SS
     - hits: (h10, gspD, 10), (h20, sctC, 20)"""
