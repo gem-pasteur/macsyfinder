466af41686634651545a2b25d2ad2254af2e1300
Bertrand <bneron@pasteur.fr>
2019-10-16 10:43:44 +0200

    :white_check_mark: fix tests according to new GeneBank api

--- tests/test_GeneBank.py
+++ tests/test_GeneBank.py
@@ -29,8 +29,7 @@ import tempfile
 import argparse
 
 from macsypy.gene import GeneBank
-from macsypy.gene import Gene
-from macsypy.profile import ProfileFactory
+from macsypy.gene import CoreGene, ModelGene
 from macsypy.model import Model
 from macsypy.config import Config, MacsyDefaults
 from macsypy.registries import ModelLocation
@@ -49,9 +48,9 @@ class Test(MacsyTest):
         self.cfg = Config(MacsyDefaults(), args)
 
         self.model_name = 'foo'
-        self.models_location = ModelLocation(path=os.path.join(args.models_dir, self.model_name))
+        self.model_location = ModelLocation(path=os.path.join(args.models_dir, self.model_name))
         self.gene_bank = GeneBank()
-        self.profile_factory = ProfileFactory(self.cfg)
+
 
     def tearDown(self):
         try:
@@ -59,46 +58,56 @@ class Test(MacsyTest):
         except:
             pass
 
+
     def test_add_get_gene(self):
         gene_name = 'sctJ_FLG'
-        self.assertRaises(KeyError, self.gene_bank.__getitem__, gene_name)
-        system_foo = Model("foo/bar", 10)
-        gene = Gene(self.profile_factory, gene_name, system_foo, self.models_location)
-        self.gene_bank.add_gene(gene)
-        gene_from_bank = self.gene_bank[(self.model_name, gene_name)]
-        self.assertTrue(isinstance(gene_from_bank, Gene))
-        self.assertEqual(gene_from_bank, gene)
         with self.assertRaises(KeyError) as ctx:
-            self.gene_bank.add_gene(gene)
+            self.gene_bank[f"foo/{gene_name}"]
         self.assertEqual(str(ctx.exception),
-                         f'"a gene named \'foo/{gene.name}\' is already registered"')
+                         f"\"No such gene 'foo/{gene_name}' in this bank\"")
+        model_foo = Model(self.model_name, 10)
+
+        profile = self.model_location.get_profile(gene_name)
+        c_gene = CoreGene(gene_name, model_foo.family_name, profile)
+        gene = ModelGene(c_gene, model_foo)
+
+        self.gene_bank.add_new_gene(self.model_location, gene_name)
+
+        gene_from_bank = self.gene_bank[(model_foo.family_name, gene_name)]
+        self.assertTrue(isinstance(gene_from_bank, CoreGene))
+        self.assertEqual(gene_from_bank, gene)
+        gbk_contains_bfore = list(self.gene_bank)
+        self.gene_bank.add_new_gene(self.model_location, gene_name)
+        gbk_contains_after = list(self.gene_bank)
+        self.assertEqual(gbk_contains_bfore, gbk_contains_after)
+
 
     def test_contains(self):
-        system_foo = Model("foo/bar", 10)
-        gene_in = Gene(self.profile_factory, 'sctJ_FLG', system_foo, self.models_location)
-        self.gene_bank.add_gene(gene_in)
+        model_foo = Model("foo/bar", 10)
+
+        gene_name = 'sctJ_FLG'
+
+        self.gene_bank.add_new_gene(self.model_location, gene_name)
+        gene_in = self.gene_bank[(model_foo.family_name, gene_name)]
         self.assertIn(gene_in, self.gene_bank)
-        gene_out = Gene(self.profile_factory, 'abc', system_foo, self.models_location)
+
+        gene_name = 'abc'
+        profile = self.model_location.get_profile(gene_name)
+        c_gene_out = CoreGene(gene_name, model_foo.family_name, profile)
+        gene_out = ModelGene(c_gene_out, model_foo)
         self.assertNotIn(gene_out, self.gene_bank)
 
+
     def test_iter(self):
-        system_foo = Model("foo/bar", 10)
-        genes = [Gene(self.profile_factory, 'sctJ_FLG', system_foo, self.models_location),
-                 Gene(self.profile_factory, 'abc', system_foo, self.models_location)]
-        for g in genes:
-            self.gene_bank.add_gene(g)
-        i = 0
-        for g in self.gene_bank:
-            self.assertIn(g, genes)
-            i += 1
-        self.assertEqual(i, len(genes))
+        genes_names = ['sctJ_FLG', 'abc']
+        for g in genes_names:
+            self.gene_bank.add_new_gene(self.model_location, g)
+        self.assertListEqual([g.name for g in self.gene_bank],
+                             genes_names)
 
 
     def test_get_uniq_object(self):
-        system_foo = Model("foo", 10)
-        gene_in = Gene(self.profile_factory, 'sctJ_FLG', system_foo, self.models_location)
-        self.gene_bank.add_gene(gene_in)
-        gene1 = self.gene_bank[(self.model_name, 'sctJ_FLG')]
-        gene2 = self.gene_bank[(self.model_name, 'sctJ_FLG')]
-        self.assertEqual(gene1, gene2)
-        self.assertIs(gene1, gene2)
+        gene_name = 'sctJ_FLG'
+        self.gene_bank.add_new_gene(self.model_location, gene_name)
+        self.gene_bank.add_new_gene(self.model_location, gene_name)
+        self.assertEqual(len(self.gene_bank), 1)
