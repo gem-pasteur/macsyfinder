49f0a41c71ba776d013cc7e11c2170654c5d1b68
Bertrand <bneron@pasteur.fr>
2020-02-19 16:03:12 +0100

    :white_check_mark: add functional tests

--- tests/test_functional_test.py
+++ tests/test_functional_test.py
@@ -32,9 +32,9 @@ import json
 
 from tests import MacsyTest, which
 from macsypy.scripts import macsyfinder
+from macsypy.error import OptionError
 
 
-@unittest.skip("skipping until macsyfinder api is not stable")
 class Test(MacsyTest):
 
     def setUp(self):
@@ -46,7 +46,7 @@ class Test(MacsyTest):
         except:
             pass
 
-
+    @unittest.skip("skipping until macsyfinder api is not stable")
     @unittest.skipIf(not which('hmmsearch'), 'hmmsearch not found in PATH')
     def test_basic_run(self):
         """
@@ -114,7 +114,7 @@ class Test(MacsyTest):
                              "\ngenes expected: {}"
                              "\ngenes retrieved: {}".format(expected_result_json['genes'], test_result_json['genes']))
 
-
+    @unittest.skip("skipping until macsyfinder api is not stable")
     def test_T2SS_ordered_circular(self):
         args = "--sequence-db {seq_db} --db-type ordered_replicon --replicon-topology {topology}  " \
                "--models-dir {models_dir} " \
@@ -127,7 +127,7 @@ class Test(MacsyTest):
                                                    )
         self._test_macsyfinder_run(args)
 
-
+    @unittest.skip("skipping until macsyfinder api is not stable")
     def test_T2SS_ordered_circular_single_locus(self):
         args = "--sequence-db {seq_db} --db-type ordered_replicon --replicon-topology {topology}  " \
                "--models-dir {models_dir} " \
@@ -139,7 +139,7 @@ class Test(MacsyTest):
                                                    )
         self._test_macsyfinder_run(args)
 
-
+    @unittest.skip("skipping until macsyfinder api is not stable")
     def test_T2SS_ordered_linear(self):
         args = "--sequence-db {seq_db} --db-type ordered_replicon --replicon-topology {topology}  " \
                "--models-dir {models_dir} " \
@@ -151,7 +151,7 @@ class Test(MacsyTest):
                                                    )
         self._test_macsyfinder_run(args)
 
-
+    @unittest.skip("skipping until macsyfinder api is not stable")
     def test_T2SS_ordered_linear_multi_loci(self):
         args = "--sequence-db {seq_db} --db-type ordered_replicon --replicon-topology {topology}  " \
                "--models-dir {models_dir} " \
@@ -165,7 +165,7 @@ class Test(MacsyTest):
                                                  )
         self._test_macsyfinder_run(args)
 
-
+    @unittest.skip("skipping until macsyfinder api is not stable")
     def test_T2SS_ordered_linear_single_locus(self):
         args = "--sequence-db {seq_db} --db-type ordered_replicon --replicon-topology {topology}  " \
                "--models-dir {models_dir} " \
@@ -177,7 +177,7 @@ class Test(MacsyTest):
                                                    )
         self._test_macsyfinder_run(args)
 
-
+    @unittest.skip("skipping until macsyfinder api is not stable")
     def test_T4P_ordered_linear(self):
         args = "--sequence-db {seq_db} --db-type ordered_replicon --replicon-topology {topology}  " \
                "--models-dir {models_dir} " \
@@ -189,7 +189,7 @@ class Test(MacsyTest):
                                                    )
         self._test_macsyfinder_run(args)
 
-
+    @unittest.skip("skipping until macsyfinder api is not stable")
     def test_T4P_ordered_linear_multi_loci(self):
         args = "--sequence-db {seq_db} --db-type ordered_replicon --replicon-topology {topology}  " \
                "--models-dir {models_dir} " \
@@ -203,12 +203,113 @@ class Test(MacsyTest):
                                                  )
         self._test_macsyfinder_run(args)
 
+
+    def test_working_dir_exists(self):
+        args = "--sequence-db {seq_db} --db-type ordered_replicon " \
+               "--models-dir {models_dir} " \
+               "-m {models} -o {{out_dir}} ".format(
+                                                 models_dir=self.find_data('functional_tests', 'models'),
+                                                 seq_db=self.find_data('functional_tests',
+                                                                       'acav001_T2SS-Ori_T4P-multi.fasta'),
+                                                 models="TXSS_modified T4P-single-locus",
+                                                 topology="linear"
+                                                 )
+        self.out_dir = os.path.join(self.tmp_dir, 'macsyfinder_working_dir_exists')
+        os.makedirs(self.out_dir)
+        open(os.path.join(self.out_dir, 'toto.empty'), 'w').close()
+
+        args = args.format(out_dir=self.out_dir)
+        with self.assertRaises(ValueError) as ctx:
+            macsyfinder.main(args=args.split(), loglevel='ERROR')
+        self.assertEqual(str(ctx.exception),
+                         f"'{self.out_dir}' already exists and is not a empty")
+
+    def test_working_dir_exists_and_not_dir(self):
+        args = "--sequence-db {seq_db} --db-type ordered_replicon " \
+               "--models-dir {models_dir} " \
+               "-m {models} -o {{out_dir}} ".format(
+                                                 models_dir=self.find_data('functional_tests', 'models'),
+                                                 seq_db=self.find_data('functional_tests',
+                                                                       'acav001_T2SS-Ori_T4P-multi.fasta'),
+                                                 models="TXSS_modified T4P-single-locus",
+                                                 topology="linear"
+                                                 )
+        self.out_dir = os.path.join(self.tmp_dir, 'macsyfinder_working_dir_exists_and_not_dir')
+        try:
+            open(self.out_dir, 'w').close()
+
+            args = args.format(out_dir=self.out_dir)
+            with self.assertRaises(ValueError) as ctx:
+                macsyfinder.main(args=args.split(), loglevel='ERROR')
+            self.assertEqual(str(ctx.exception),
+                             f"'{self.out_dir}' already exists and is not a directory")
+        finally:
+            os.unlink(self.out_dir)
+
+
+    def test_no_models(self):
+        args = "--sequence-db {seq_db} --db-type ordered_replicon " \
+               "--models-dir {models_dir} " \
+               "-o {{out_dir}} ".format(
+                                         models_dir=self.find_data('functional_tests', 'models'),
+                                         seq_db=self.find_data('functional_tests',
+                                                               'acav001_T2SS-Ori_T4P-multi.fasta'),
+                                         topology="linear"
+                                        )
+        self.out_dir = os.path.join(self.tmp_dir, 'macsyfinder_no_models')
+        args = args.format(out_dir=self.out_dir)
+        with self.catch_io(out=True):
+            with self.assertRaises(OptionError) as ctx:
+                macsyfinder.main(args=args.split(), loglevel='ERROR')
+        self.assertEqual(str(ctx.exception),
+                         "argument --models or --previous-run is required.")
+
+    def test_no_seq_db(self):
+        args = "--db-type ordered_replicon " \
+               "--models-dir {models_dir} " \
+               "-o {{out_dir}} " \
+               "-m {models} -o {{out_dir}} ".format(
+                                                     models_dir=self.find_data('functional_tests', 'models'),
+                                                     models="TXSS_modified T4P-single-locus",
+                                                     topology="linear"
+                                                     )
+        self.out_dir = os.path.join(self.tmp_dir, 'macsyfinder_no_seq_db')
+
+        args = args.format(out_dir=self.out_dir)
+        with self.catch_io(out=True):
+            with self.assertRaises(OptionError) as ctx:
+                macsyfinder.main(args=args.split(), loglevel='ERROR')
+        self.assertEqual(str(ctx.exception),
+                         "argument --sequence-db or --previous-run is required.")
+
+    def test_no_db_type(self):
+        args = "--sequence-db {seq_db} " \
+               "--models-dir {models_dir} " \
+               "-o {{out_dir}} "\
+               "-m {models} -o {{out_dir}} ".format(seq_db=self.find_data('functional_tests',
+                                                                          'acav001_T2SS-Ori_T4P-multi.fasta'),
+                                                    models_dir=self.find_data('functional_tests', 'models'),
+                                                    models="TXSS_modified T4P-single-locus",
+                                                    topology="linear"
+                                                    )
+        self.out_dir = os.path.join(self.tmp_dir, 'macsyfinder_no_db_type')
+        os.makedirs(self.out_dir)
+
+        args = args.format(out_dir=self.out_dir)
+        with self.catch_io(out=True):
+            with self.assertRaises(OptionError) as ctx:
+                macsyfinder.main(args=args.split(), loglevel='ERROR')
+        self.assertEqual(str(ctx.exception),
+                         "argument --db-type or --previous-run is required.")
+
+
     def _test_macsyfinder_run(self, args_tpl):
         # get the name of the calling function
         test_name = inspect.stack()[1].function
         self.out_dir = os.path.join(self.tmp_dir, 'macsyfinder_{}'.format(test_name))
         os.makedirs(self.out_dir)
         args = args_tpl.format(out_dir=self.out_dir)
+
         macsyfinder.main(args=args.split(),
                          loglevel='ERROR'
                          )
