cf0a770719d5528d3dfce9e393cc02ee6b2ff539
Bertrand Néron <bneron@pasteur.fr>
2020-04-02 10:56:27 +0200

    :pencil2: add apidoc about scoring systems

--- macsypy/score.py
+++ macsypy/score.py
@@ -26,8 +26,26 @@ from macsypy.error import MacsypyError
 
 
 class ComposedScore:
+    """
+    Modelize a system score based on several dimensions
+
+        - the system score itself
+        - the number of overlapping genes (number of genes which are represented several times)
+        - the length of overlapping (number of occurrence of overlapping genes)
+
+          *\- H1\- \- H2\- \- H3\- \- H1 \- \- H2' \- \- H1 \-* (H2' code for a exchangeable of H2)
+
+        - overlapping genes = H1 + H2 = 2
+        - overlapping length = 3 H1 + 2  H2 = 5
+    """
 
     def __init__(self, system, hit_tracker):
+        """
+        :param system: the system to compute the composed score
+        :type system: :class:`macsypy.system.System` object
+        :param hit_tracker: the hit tracker build for this run
+        :type hit_tracker: :class:`macsypy.hit.HitTracker` object
+        """
         self._system = system
         self._hit_tracker = hit_tracker
         self._sys_score = self._system.score
@@ -36,6 +54,9 @@ class ComposedScore:
         self._overlap()
 
     def _overlap(self):
+        """
+        Compute overlapping genes and overlapping length
+        """
         used_in_systems = {}
         for vh in self._system.hits:
             used_in_systems[vh] = [s.id for s in self._hit_tracker[vh.hit] if s.model.fqn != self._system.model.fqn]
@@ -44,25 +65,51 @@ class ComposedScore:
 
     @property
     def system(self):
+        """
+        :return: The system related to this score
+        :rtype: :class:`macsypy.system.System`
+        """
         return self._system
 
     @property
     def sys_score(self):
+        """
+        :return: the system score itself
+        :rtype: float
+        """
         return self.sys_score()
 
 
     @property
     def overlapping_genes(self):
+        """
+        :return: The number of overlapping genes (number of genes which are represented several times)
+        :rtype: int
+        """
         return self._overlapping_genes
 
     @property
     def overlapping_length(self):
+        """
+        :return: The length of overlapping (number of occurrence of overlapping genes)
+        :rtype: int
+        """
         return self._overlapping_length
 
 
 class BestSystemSelector:
+    """
+    Sort systems and filter them to proposed only the best ones
+    """
 
     def __init__(self, systems, hit_tracker):
+        """
+
+        :param systems: The systems to sort, filter. The systems must be occurrence of the same model
+        :type systems: list of :class:`macsypy.system.System` object
+        :param hit_tracker: the hit tracker build for this run
+        :type hit_tracker: :class:`macsypy.hit.HitTracker` object
+        """
         models = {sys.model.fqn for sys in systems}
         if len(models) != 1:
             raise MacsypyError(f"Cannot build Score with system from different models: {','.join(models)}")
@@ -70,6 +117,18 @@ class BestSystemSelector:
         self.hit_tracker = hit_tracker
 
     def best_system(self):
+        """
+        :return: the best system
+        :rtype: list of :class:`macsypy.system.System` objects
+
+        best system use an heuristic to select the best systems.
+
+            1. select the systems with the highest score
+            2. if several systems have same score, choose among them
+               the system with the fewest different overlapping genes (:attr:`macsypy.score.ComposedScore.overlapping_genes`)
+            3. if there is still several systems, pick the systems
+               with the fewest number of genes overlapping (:attr:`macsypy.score.ComposedScore.overlapping_length`)
+        """
         systems = sorted(self.systems, key=lambda s: - s.score)
         grouped = itertools.groupby(systems, key=lambda s: s.score)
         score, best_systems = next(grouped)
