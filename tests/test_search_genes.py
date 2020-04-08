a696e65ede701e19c50bdf75dad9029d511c45e2
Bertrand Néron <bneron@pasteur.fr>
2020-04-08 09:40:05 +0200

    :white_check_mark: increase search_genes unit tests coverage

--- tests/test_search_genes.py
+++ tests/test_search_genes.py
@@ -42,16 +42,19 @@ from tests import MacsyTest, which
 class TestSearchGenes(MacsyTest):
 
     def setUp(self):
+        self.tmp_dir = os.path.join(tempfile.gettempdir(),
+                                    'test_macsyfinder_search_genes')
+        if os.path.exists(self.tmp_dir):
+            shutil.rmtree(self.tmp_dir)
+        os.mkdir(self.tmp_dir)
+
         args = argparse.Namespace()
         args.sequence_db = self.find_data("base", "test_base.fa")
         args.db_type = 'gembase'
         args.models_dir = self.find_data('models')
-        args.res_search_dir = tempfile.gettempdir()
         args.log_level = 30
-        args.out_dir = os.path.join(args.res_search_dir,
-                                    'test_macsyfinder_search_genes')
-        if os.path.exists(args.out_dir):
-            shutil.rmtree(args.out_dir)
+        args.out_dir = os.path.join(self.tmp_dir, 'job_1')
+        args.res_search_dir = args.out_dir
         os.mkdir(args.out_dir)
 
         self.cfg = Config(MacsyDefaults(), args)
@@ -66,7 +69,7 @@ class TestSearchGenes(MacsyTest):
     def tearDown(self):
         try:
             shutil.rmtree(self.cfg.working_dir())
-            pass
+            #pass
         except:
             pass
 
@@ -79,5 +82,33 @@ class TestSearchGenes(MacsyTest):
         expected_hit = [Hit(c_gene_abc, "ESCO030p01_000260", 706, "ESCO030p01",
                             26, float(1.000e-200), float(660.800), float(1.000), float(0.714), 160, 663
                             )]
+        self.assertEqual(len(report), 1)
+        self.assertEqual(expected_hit[0], report[0].hits[0])
+
+
+    @unittest.skipIf(not which('hmmsearch'), 'hmmsearch not found in PATH')
+    def test_search_recover(self):
+        # first job searching using hmmsearch
+        gene_name = "abc"
+        c_gene_abc = CoreGene(self.model_location, gene_name, self.profile_factory)
+        report = search_genes([c_gene_abc], self.cfg)
+        expected_hit = [Hit(c_gene_abc, "ESCO030p01_000260", 706, "ESCO030p01",
+                            26, float(1.000e-200), float(660.800), float(1.000), float(0.714), 160, 663
+                            )]
+
+        # second job using recover
+        # disable hmmer to be sure that test use the recover inner function
+        self.cfg.hmmer = lambda: "hmmer_disable"
+        # and create a new dir for the second job
+        previous_job_path = self.cfg.working_dir()
+        self.cfg.previous_run = lambda: previous_job_path
+        self.cfg.out_dir = lambda: os.path.join(self.tmp_dir, 'job_2')
+        os.mkdir(self.cfg.out_dir())
+
+        # rerun with previous run
+        # but we have to reset the profile attached to the gene gene._profile._report
+        self.profile_factory = ProfileFactory(self.cfg)
+        c_gene_abc = CoreGene(self.model_location, gene_name, self.profile_factory)
+        report = search_genes([c_gene_abc], self.cfg)
         self.assertEqual(len(report), 1)
         self.assertEqual(expected_hit[0], report[0].hits[0])
