b16e279169672095fd4cda65475c21d05ab56457
Bertrand Néron <bneron@pasteur.fr>
2020-12-11 11:02:42 +0100

    use inskape to conver svg to png

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -39,7 +39,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
               'sphinx.ext.inheritance_diagram',
               'sphinx.ext.graphviz',
-              'sphinx.ext.imgconverter'
+              'sphinxcontrib.inkscapeconverter'
               ]
 
 # Add any paths that contain templates here, relative to this directory.
