01306223b58e8d77ecf3603f5767c3bd13caa105
Bertrand Néron <bneron@pasteur.fr>
2021-01-25 18:00:42 +0100

    :white_check_mark: add unit test about macsyprofile script

---
+++ tests/test_macsyprofile.py
@@ -0,0 +1,242 @@
+#########################################################################
+# MacSyFinder - Detection of macromolecular systems in protein dataset  #
+#               using systems modelling and similarity search.          #
+# Authors: Sophie Abby, Bertrand Neron                                  #
+# Copyright (c) 2014-2020  Institut Pasteur (Paris) and CNRS.           #
+# See the COPYRIGHT file for details                                    #
+#                                                                       #
+# This file is part of MacSyFinder package.                             #
+#                                                                       #
+# MacSyFinder is free software: you can redistribute it and/or modify   #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# MacSyFinder is distributed in the hope that it will be useful,        #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+# GNU General Public License for more details .                         #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with MacSyFinder (COPYING).                                     #
+# If not, see <https://www.gnu.org/licenses/>.                          #
+#########################################################################
+
+import tempfile
+import shutil
+import os
+import argparse
+import sys
+
+import macsypy
+from macsypy.config import MacsyDefaults, Config
+from macsypy.database import Indexes
+from tests import MacsyTest
+from macsypy.scripts import macsyprofile
+
+
+class TestMacsyprofile(MacsyTest):
+
+    def setUp(self):
+        self.tmpdir = tempfile.mkdtemp()
+        self.models_dir = os.path.join(self.tmpdir, 'models')
+        os.mkdir(self.models_dir)
+
+        self.args = argparse.Namespace()
+        macsyprofile._log = macsyprofile.init_logger(20)  # 20 logging.INFO
+        self.previous_run = self.find_data('functional_test_gembase')
+
+    def tearDown(self):
+        try:
+            shutil.rmtree(self.tmpdir)
+        except:
+            pass
+        # some function in macsyprofile script suppress the traceback
+        # but without traceback it's hard to debug test :-(
+        sys.tracebacklimit = 1000  # the default value
+
+
+    def test_pasre_args(self):
+        cmd = f"macsyprofile {self.previous_run}"
+        args = macsyprofile.parse_args(cmd.split()[1:])
+        self.assertEqual(args.previous_run, self.previous_run)
+        self.assertEqual(args.coverage_profile, -1.)
+        self.assertEqual(args.i_evalue_sel, 1.0e9)
+        self.assertIsNone(args.best_hits)
+        self.assertEqual(args.pattern, '*')
+        self.assertIsNone(args.out)
+        self.assertFalse(args.force)
+        self.assertEqual(args.verbosity, 0)
+        self.assertFalse(args.mute)
+
+        cmd = f"macsyprofile -vv {self.previous_run}"
+        args = macsyprofile.parse_args(cmd.split()[1:])
+        self.assertEqual(args.verbosity, 2)
+
+        cmd = f"macsyprofile -f {self.previous_run}"
+        args = macsyprofile.parse_args(cmd.split()[1:])
+        self.assertTrue(args.force)
+
+        cmd = f"macsyprofile --out toto {self.previous_run}"
+        args = macsyprofile.parse_args(cmd.split()[1:])
+        self.assertEqual(args.out, 'toto')
+
+        pattern = '?toto'
+        cmd = f"macsyprofile --pattern {pattern} {self.previous_run}"
+        args = macsyprofile.parse_args(cmd.split()[1:])
+        self.assertEqual(args.pattern, pattern)
+
+        cmd = f"macsyprofile --best-hits score {self.previous_run}"
+        args = macsyprofile.parse_args(cmd.split()[1:])
+        self.assertEqual(args.best_hits, 'score')
+
+        cmd = f"macsyprofile --i-evalue-sel 1.0 {self.previous_run}"
+        args = macsyprofile.parse_args(cmd.split()[1:])
+        self.assertEqual(args.i_evalue_sel, 1.0)
+
+        cmd = f"macsyprofile --coverage-profile 1.0 {self.previous_run}"
+        args = macsyprofile.parse_args(cmd.split()[1:])
+        self.assertEqual(args.coverage_profile, 1.0)
+
+
+    def test_verbosity_to_log_level(self):
+        level = macsyprofile.verbosity_to_log_level(1)
+        self.assertEqual(level, 10)
+        level = macsyprofile.verbosity_to_log_level(5)
+        self.assertEqual(level, 1)
+
+
+    def test_header(self):
+        out = "FOO"
+        coverage_profile = 0.1
+        version = macsypy.__version__
+        cmd = f"macsyprofile --coverage-profile {coverage_profile} --out {out} {self.previous_run}"
+        expected_header = f"""# macsyprofile {version}
+# macsyprofile {' '.join(cmd.split()[1:])}
+
+hit_id\treplicon_name\tposition_hit\thit_sequence_length\tgene_name\ti_eval\tscore\tprofile_coverage\tsequence_coverage
+\tbegin\tend"""
+        got_header = macsyprofile.header(cmd.split()[1:])
+        self.assertEqual(expected_header, got_header)
+
+
+    def test_get_gene_name(self):
+        expected_gene_name = "Archaeal-T4P_arCOG01822"
+        path = f"tests/data/functional_test_gembase/hmmer_results/{expected_gene_name}.res_hmm_extract"
+        suffix = ".res_hmm_extract"
+        self.assertEqual(macsyprofile.get_gene_name(path, suffix), expected_gene_name)
+
+
+    def test_getProfile_len(self):
+        path = self.find_data('models', 'functional', 'profiles', 'T1SS_abc.hmm')
+        self.assertEqual(macsyprofile.get_profile_len(path), 501)
+
+
+    def test_LightHit(self):
+        data = {'id': 'GCF_000006745_003720',
+                'replicon_name': 'GCF_000006745',
+                'position': 372,
+                'seq_length': 196,
+                'gene_name': 'T4bP_pilA',
+                'i_eval': 2.000e+01,
+                'score': 3.400,
+                'profile_coverage': 0.219,
+                'sequence_coverage': 0.153,
+                'begin_match': 147,
+                'end_match': 176,
+                 }
+        l_hit = macsyprofile.LightHit(data['gene_name'],
+                                      data['id'],
+                                      data['seq_length'],
+                                      data['replicon_name'],
+                                      data['position'],
+                                      data['i_eval'],
+                                      data['score'],
+                                      data['profile_coverage'],
+                                      data['sequence_coverage'],
+                                      data['begin_match'],
+                                      data['end_match'],
+                                      )
+        exp_str = "GCF_000006745_003720\tGCF_000006745\t372\t196\tT4bP_pilA\t2.000e+01\t3.400\t0.219\t0.153\t147\t176"
+        self.assertEqual(str(l_hit), exp_str)
+
+
+    def test_build_my_db(self):
+        gene_name = "gspD"
+        args = argparse.Namespace()
+        args.db_type = 'gembase'
+        args.models_dir = self.find_data('models')
+        args.log_level = 30
+        self.cfg = Config(MacsyDefaults(), args)
+        gspD_hmmer_path = self.find_data(os.path.join('hmm', 'gspD.search_hmm.out'))
+
+        hmm_prof = macsyprofile.HmmProfile(gene_name, 596, gspD_hmmer_path, self.cfg)
+
+        db = hmm_prof._build_my_db(gspD_hmmer_path)
+        self.assertDictEqual(db, {'PSAE001c01_031420': None,
+                                  'PSAE001c01_051090': None,
+                                  'PSAE001c01_018920': None,
+                                  'PSAE001c01_043580': None,
+                                  'PSAE001c01_017350': None,
+                                  'PSAE001c01_013980': None,
+                                  'PSAE001c01_026600': None,
+                                  'NC_xxxxx_xx_056141': None,
+                                  'PSAE001c01_006940': None})
+
+
+    def test_fill_my_db(self):
+        gene_name = "gspD"
+        args = argparse.Namespace()
+        args.db_type = 'gembase'
+        args.models_dir = self.find_data('models')
+        args.log_level = 30
+        args.sequence_db = self.find_data("base", "test_base.fa")
+        cfg = Config(MacsyDefaults(), args)
+        gspD_hmmer_path = self.find_data(os.path.join('hmm', 'gspD.search_hmm.out'))
+
+        idx = Indexes(cfg)
+        macsyfinder_idx = idx.find_my_indexes()
+        hmm_prof = macsyprofile.HmmProfile(gene_name, 596, gspD_hmmer_path, cfg)
+
+        db = hmm_prof._build_my_db(gspD_hmmer_path)
+        hmm_prof._fill_my_db(macsyfinder_idx, db)
+        self.assertDictEqual(db, {'PSAE001c01_031420': (658, 73),
+                                  'PSAE001c01_051090': (714, 75),
+                                  'PSAE001c01_018920': (776, 71),
+                                  'PSAE001c01_043580': (416, 74),
+                                  'PSAE001c01_017350': (600, 70),
+                                  'PSAE001c01_013980': (759, 69),
+                                  'PSAE001c01_026600': (273, 72),
+                                  'NC_xxxxx_xx_056141': (803, 141),
+                                  'PSAE001c01_006940': (803, 68)})
+
+
+    def test_hit_start(self):
+        gene_name = "gspD"
+        args = argparse.Namespace()
+        args.db_type = 'gembase'
+        args.models_dir = self.find_data('models')
+        args.log_level = 30
+        self.cfg = Config(MacsyDefaults(), args)
+        gspD_hmmer_path = self.find_data(os.path.join('hmm', 'gspD.search_hmm.out'))
+
+        hmm_prof = macsyprofile.HmmProfile(gene_name, 596, gspD_hmmer_path, self.cfg)
+
+        self.assertFalse(hmm_prof._hit_start("NOT starting hit"))
+        self.assertTrue(hmm_prof._hit_start(">> starting hit"))
+
+
+    def test_parse_hmm_header(self):
+        gene_name = "gspD"
+        args = argparse.Namespace()
+        args.db_type = 'gembase'
+        args.models_dir = self.find_data('models')
+        args.log_level = 30
+        self.cfg = Config(MacsyDefaults(), args)
+        gspD_hmmer_path = self.find_data(os.path.join('hmm', 'gspD.search_hmm.out'))
+
+        hmm_prof = macsyprofile.HmmProfile(gene_name, 596, gspD_hmmer_path, self.cfg)
+
+        hmm_hit = [">> NC_xxxxx_xx_056141  C ATG TAA 6260390 6261757 Valid PA5567 1368 _NP_254254.1_ PA5567 1 6260390 6261757 | tRNA modific"]
+        hit_id = hmm_prof._parse_hmm_header(hmm_hit)
+        self.assertEqual(hit_id, 'NC_xxxxx_xx_056141')
