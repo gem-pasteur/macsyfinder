0f0f3e680238aab9acaaa7559c84ce801c6fd217
Bertrand Néron <bneron@pasteur.fr>
2021-10-13 15:02:10 +0200

    :white_check_mark: unit tests for Loner

--- tests/test_hit.py
+++ tests/test_hit.py
@@ -25,7 +25,7 @@
 import os
 import argparse
 
-from macsypy.hit import CoreHit, ModelHit, get_best_hits, HitWeight
+from macsypy.hit import CoreHit, ModelHit, Loner, get_best_hits, get_best_hit_4_func, HitWeight
 from macsypy.config import Config, MacsyDefaults
 from macsypy.gene import CoreGene, ModelGene, GeneStatus
 from macsypy.profile import ProfileFactory
@@ -165,50 +165,134 @@ class ModelHitTest(MacsyTest):
         model.add_mandatory_gene(self.gene_gspd)
         model.add_accessory_gene(self.gene_sctj)
 
-        self.hit_1 = CoreHit(self.c_gene_gspd, "hit_1", 803, "replicon_id", 2, 1.0, 1.0, 1.0, 1.0, 10, 20)
-        self.hit_2 = CoreHit(self.c_gene_sctj, "hit_2", 803, "replicon_id", 3, 1.0, 1.0, 1.0, 1.0, 10, 20)
+        self.chit_1 = CoreHit(self.c_gene_gspd, "hit_1", 803, "replicon_id", 2, 1.0, 1.0, 1.0, 1.0, 10, 20)
+        self.chit_2 = CoreHit(self.c_gene_sctj, "hit_2", 803, "replicon_id", 3, 1.0, 1.0, 1.0, 1.0, 10, 20)
 
 
     def test_init(self):
-        v_hit_1 = ModelHit(self.hit_1, self.gene_gspd, GeneStatus.MANDATORY)
-        self.assertEqual(v_hit_1.gene_ref, self.gene_gspd)
-        self.assertEqual(v_hit_1.status, GeneStatus.MANDATORY)
-        v_hit_2 = ModelHit(self.hit_2, self.gene_gspd, GeneStatus.ACCESSORY)
-        self.assertEqual(v_hit_2.gene_ref, self.gene_gspd)
-        self.assertEqual(v_hit_2.status, GeneStatus.ACCESSORY)
+        mhit_1 = ModelHit(self.chit_1, self.gene_gspd, GeneStatus.MANDATORY)
+        self.assertEqual(mhit_1.gene_ref, self.gene_gspd)
+        self.assertEqual(mhit_1.status, GeneStatus.MANDATORY)
+        mhit_2 = ModelHit(self.chit_2, self.gene_gspd, GeneStatus.ACCESSORY)
+        self.assertEqual(mhit_2.gene_ref, self.gene_gspd)
+        self.assertEqual(mhit_2.status, GeneStatus.ACCESSORY)
 
         with self.assertRaises(MacsypyError) as ctx:
-            ModelHit(self.hit_1, self.c_gene_gspd, GeneStatus.MANDATORY)
+            ModelHit(self.chit_1, self.c_gene_gspd, GeneStatus.MANDATORY)
         self.assertEqual(str(ctx.exception),
                          "The ModelHit 'gene_ref' argument must be a ModelGene not <class 'macsypy.gene.CoreGene'>.")
 
     def test_hash(self):
-        self.assertEqual(hash(self.hit_1), hash(self.hit_1))
-        self.assertNotEqual(hash(self.hit_1), hash(self.hit_2))
+        self.assertEqual(hash(self.chit_1), hash(self.chit_1))
+        self.assertNotEqual(hash(self.chit_1), hash(self.chit_2))
 
     def test_delegation(self):
-        v_hit_1 = ModelHit(self.hit_1, self.gene_gspd, GeneStatus.MANDATORY)
-        self.assertEqual(v_hit_1.get_position(), 2)
+        mhit_1 = ModelHit(self.chit_1, self.gene_gspd, GeneStatus.MANDATORY)
+        self.assertEqual(mhit_1.get_position(), 2)
+        with self.assertRaises(AttributeError) as ctx:
+            mhit_1.nimportnaoik()
+        self.assertEqual(str(ctx.exception),
+                         "'ModelHit' object has no attribute 'nimportnaoik'")
 
     def test_eq(self):
-        v_hit_0 = ModelHit(self.hit_1, self.gene_sctj, GeneStatus.MANDATORY)
-        v_hit_1 = ModelHit(self.hit_1, self.gene_sctj, GeneStatus.MANDATORY)
-        v_hit_2 = ModelHit(self.hit_2, self.gene_gspd, GeneStatus.ACCESSORY)
+        mhit_0 = ModelHit(self.chit_1, self.gene_sctj, GeneStatus.MANDATORY)
+        mhit_1 = ModelHit(self.chit_1, self.gene_sctj, GeneStatus.MANDATORY)
+        mhit_2 = ModelHit(self.chit_2, self.gene_gspd, GeneStatus.ACCESSORY)
 
-        self.assertEqual(v_hit_0, v_hit_1)
-        self.assertNotEqual(v_hit_0, v_hit_2)
+        self.assertEqual(mhit_0, mhit_1)
+        self.assertNotEqual(mhit_0, mhit_2)
 
     def test_multi_system(self):
-        v_hit_1 = ModelHit(self.hit_1, self.gene_sctj, GeneStatus.MANDATORY)
-        v_hit_2 = ModelHit(self.hit_2, self.gene_gspd, GeneStatus.MANDATORY)
-        self.assertTrue(v_hit_2.multi_system)
-        self.assertFalse(v_hit_1.multi_system)
+        mhit_1 = ModelHit(self.chit_1, self.gene_sctj, GeneStatus.MANDATORY)
+        mhit_2 = ModelHit(self.chit_2, self.gene_gspd, GeneStatus.MANDATORY)
+        self.assertTrue(mhit_2.multi_system)
+        self.assertFalse(mhit_1.multi_system)
 
-    # def test_loner(self):
-    #     v_hit_1 = ModelHit(self.hit_1, self.gene_sctj, GeneStatus.MANDATORY)
-    #     v_hit_2 = ModelHit(self.hit_2, self.gene_gspd, GeneStatus.MANDATORY)
-    #     self.assertFalse(v_hit_2.loner)
-    #     self.assertTrue(v_hit_1.loner)
+    def test_loner(self):
+        mhit_2 = ModelHit(self.chit_2, self.gene_gspd, GeneStatus.MANDATORY)
+        self.assertFalse(mhit_2.loner)
+
+
+class LonerHitTest(MacsyTest):
+
+    def setUp(self) -> None:
+        args = argparse.Namespace()
+        args.sequence_db = self.find_data("base", "test_1.fasta")
+        args.db_type = 'gembase'
+        args.models_dir = self.find_data('models')
+        cfg = Config(MacsyDefaults(), args)
+
+        model_name = 'foo'
+        models_location = ModelLocation(path=os.path.join(args.models_dir, model_name))
+
+        model = Model("foo/T2SS", 10)
+        profile_factory = ProfileFactory(cfg)
+
+        gene_name = "gspD"
+        self.c_gene_gspd = CoreGene(models_location, gene_name, profile_factory)
+        self.gene_gspd = ModelGene(self.c_gene_gspd, model, loner=True)
+
+        gene_name = "sctJ"
+        self.c_gene_sctj = CoreGene(models_location, gene_name, profile_factory)
+        self.gene_sctj = ModelGene(self.c_gene_sctj, model)
+
+        model.add_mandatory_gene(self.gene_gspd)
+        model.add_accessory_gene(self.gene_sctj)
+
+        self.chit_1 = CoreHit(self.c_gene_gspd, "hit_1", 803, "replicon_id", 2, 1.0, 1.0, 1.0, 1.0, 10, 20)
+        self.chit_2 = CoreHit(self.c_gene_sctj, "hit_2", 803, "replicon_id", 3, 1.0, 1.0, 1.0, 1.0, 10, 20)
+        self.chit_3 = CoreHit(self.c_gene_gspd, "hit_3", 803, "replicon_id", 10, 1.0, 1.0, 1.0, 1.0, 10, 20)
+
+
+    def test_init(self):
+        # create loner from a CoreHit
+        l1 = Loner(self.chit_1, gene_ref=self.gene_gspd, gene_status=GeneStatus.MANDATORY)
+        self.assertEqual(l1.gene_ref, self.gene_gspd)
+        self.assertEqual(l1.status, GeneStatus.MANDATORY)
+        # create loner from a CoreHit with counterpart
+        l2 = Loner(self.chit_1, gene_ref=self.gene_gspd, gene_status=GeneStatus.MANDATORY, counterpart=[self.chit_3])
+
+        # create loner from a ModelHit
+        mh1 = ModelHit(self.chit_1, self.gene_gspd, GeneStatus.MANDATORY)
+        l3 = Loner(mh1)
+        self.assertEqual(l3.gene_ref, self.gene_gspd)
+        self.assertEqual(l3.status, GeneStatus.MANDATORY)
+
+        with self.catch_log(log_name='macsypy') as log:
+            with self.assertRaises(MacsypyError) as ctx:
+                Loner(self.chit_2, gene_ref=self.gene_sctj, gene_status=GeneStatus.ACCESSORY)
+        self.assertEqual(str(ctx.exception),
+                         "hit_2 cannot be a loner gene_ref 'sctJ' not tag as loner")
+
+        with self.assertRaises(MacsypyError) as ctx:
+            ModelHit(self.chit_1, self.c_gene_gspd, GeneStatus.MANDATORY)
+        self.assertEqual(str(ctx.exception),
+                         "The ModelHit 'gene_ref' argument must be a ModelGene not <class 'macsypy.gene.CoreGene'>.")
+
+
+    def test_counterpart(self):
+        l1 = Loner(self.chit_1, gene_ref=self.gene_gspd, gene_status=GeneStatus.MANDATORY)
+        self.assertListEqual(l1.counterpart, [])
+        l2 = Loner(self.chit_1, gene_ref=self.gene_gspd, gene_status=GeneStatus.MANDATORY, counterpart=[self.chit_3])
+        self.assertListEqual(l2.counterpart, [self.chit_3])
+
+    def test_len(self):
+        l1 = Loner(self.chit_1, gene_ref=self.gene_gspd, gene_status=GeneStatus.MANDATORY)
+        self.assertEqual(len(l1), 1)
+        l2 = Loner(self.chit_1, gene_ref=self.gene_gspd, gene_status=GeneStatus.MANDATORY, counterpart=[self.chit_3])
+        self.assertEqual(len(l2), 2)
+
+
+    def test_loner(self):
+        l1 = Loner(self.chit_1, gene_ref=self.gene_gspd, gene_status=GeneStatus.MANDATORY)
+        self.assertTrue(l1.loner)
+
+    def test_str(self):
+        l2 = Loner(self.chit_1, gene_ref=self.gene_gspd, gene_status=GeneStatus.MANDATORY, counterpart=[self.chit_3])
+        self.assertEqual(str(l2),
+                         '\t'.join(['hit_1', 'replicon_id', '2', '803', 'gspD', '1.000e+00',
+                                    '1.000', '1.000', '1.000', '10', '20', 'hit_3'])
+                         )
 
 
 class GetBestHitTest(MacsyTest):
@@ -274,6 +358,60 @@ class GetBestHitTest(MacsyTest):
         # bad criterion
         with self.assertRaises(MacsypyError) as ctx:
             get_best_hits([h0, h1], key='nimportnaoik')
+        self.assertEqual('The criterion for Hits comparison nimportnaoik does not exist or is not available.\n'
+                         'It must be either "score", "i_eval" or "profile_coverage".', str(ctx.exception))
+
+
+    def test_get_best_hits_4_func(self):
+        model = Model("foo/T2SS", 10)
+        gene_name = "gspD"
+        c_gene_gspd = CoreGene(self.models_location, gene_name, self.profile_factory)
+        gene_gspd = ModelGene(c_gene_gspd, model, loner=True)
+
+        #        gene, model, id,            hit_seq_len, replicon_name, position, i_eval,
+        #        score,      profil_coverage,      sequence_coverage,     begin,end
+        ######################
+        # based on the score #
+        ######################
+        h0 = CoreHit(gene_gspd, "PSAE001c01_006940", 803, "PSAE001c01", 3450, float(1.2e-234),
+                     10, float(1.000000), (741.0 - 104.0 + 1) / 803, 104, 741)
+        h1 = CoreHit(gene_gspd, "PSAE001c01_013980", 759, "PSAE001c01", 3450, float(3.7e-76),
+                     11, float(1.000000), (736.0 - 105.0 + 1) / 759, 105, 736)
+
+        l0 = Loner(h0, gene_ref=gene_gspd, gene_status=GeneStatus.ACCESSORY, counterpart=[h1])
+        l1 = Loner(h1, gene_ref=gene_gspd, gene_status=GeneStatus.ACCESSORY, counterpart=[h0])
+        l = get_best_hit_4_func(gene_name, [l0, l1])
+        self.assertEqual(l, l1)
+
+        #######################
+        # based on the i_eval #
+        #######################
+        h0 = CoreHit(gene_gspd, "PSAE001c01_006940", 803, "PSAE001c01", 3450, 10,
+                     10, float(1.000000), (741.0 - 104.0 + 1) / 803, 104, 741)
+        h1 = CoreHit(gene_gspd, "PSAE001c01_013980", 759, "PSAE001c01", 3450, 11,
+                     10, float(1.000000), (736.0 - 105.0 + 1) / 759, 105, 736)
+        l0 = Loner(h0, gene_ref=gene_gspd, gene_status=GeneStatus.ACCESSORY, counterpart=[h1])
+        l1 = Loner(h1, gene_ref=gene_gspd, gene_status=GeneStatus.ACCESSORY, counterpart=[h0])
+
+        l = get_best_hit_4_func(gene_name, [l0, l1], key='i_eval')
+        self.assertEqual(l, l0)
+
+        #################################
+        # based on the profile_coverage #
+        #################################
+        h0 = CoreHit(gene_gspd, "PSAE001c01_006940", 803, "PSAE001c01", 3450, 10,
+                     10, 10, (741.0 - 104.0 + 1) / 803, 104, 741)
+        h1 = CoreHit(gene_gspd, "PSAE001c01_013980", 759, "PSAE001c01", 3450, 10,
+                     10, 11, (736.0 - 105.0 + 1) / 759, 105, 736)
+        l0 = Loner(h0, gene_ref=gene_gspd, gene_status=GeneStatus.ACCESSORY, counterpart=[h1])
+        l1 = Loner(h1, gene_ref=gene_gspd, gene_status=GeneStatus.ACCESSORY, counterpart=[h0])
+
+        l = get_best_hit_4_func(gene_name, [l0, l1], key='profile_coverage')
+        self.assertEqual(l, l1)
+
+        # bad criterion
+        with self.assertRaises(MacsypyError) as ctx:
+            get_best_hits([l0, l1], key='nimportnaoik')
         self.assertEqual('The criterion for Hits comparison nimportnaoik does not exist or is not available.\n'
                          'It must be either "score", "i_eval" or "profile_coverage".', str(ctx.exception))
 
