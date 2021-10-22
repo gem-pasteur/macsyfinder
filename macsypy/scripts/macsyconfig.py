3f05722558207cf045659d370c380e3630896102
Bertrand Néron <bneron@pasteur.fr>
2021-10-22 09:31:59 +0200

    idea about config generator

---
+++ macsypy/scripts/macsyconfig.py
@@ -0,0 +1,24 @@
+
+
+opt_2_configure = {'hmmer': ('hmer'),
+                   'directories': ('models_dir', 'system_models_dir', 'out_dir', 'profile_suffix', 'res_search_dir',
+                                 'res_search_suffix', 'res_extract_suffix', 'index_dir'),
+                   'general': ('verbosity', 'worker')}
+
+default_opts = {}
+
+
+def prompt(message):
+    """
+    affiche un prompt
+    recupere une valeur
+    """
+    pass
+
+def validate(value):
+    """
+    valide une valeur par rapport a un parametre
+    :param value:
+    :return:
+    """
+
