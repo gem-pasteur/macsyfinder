69fe63e4ee1d43762cff98427c4eb71984b28bf8
Bertrand Néron <bneron@pasteur.fr>
2022-01-19 11:05:34 +0100

    :bug: fix registry bug
    the registry was not able to manage definitions tree if the tree was up
    to 2 levels
    It was due to a bug in the definitionLocation fqn attribute

--- macsypy/utils.py
+++ macsypy/utils.py
@@ -21,8 +21,7 @@
 # along with MacSyFinder (COPYING).                                     #
 # If not, see <https://www.gnu.org/licenses/>.                          #
 #########################################################################
-
-
+import os.path
 from itertools import groupby
 
 from .registries import DefinitionLocation
@@ -32,25 +31,20 @@ def get_def_to_detect(models, model_registry):
     """
     :param models: the list of models to detect as returned by config.models.
     :type models: list of tuple with the following structure:
-                  [('model_1', ('def1, def2, ...)), ('model_2', ('def1', ...)), ...]
+                  [('model_fqn', ('def1, def2, ...)), ('model_2', ('def1', ...)), ...]
     :param model_registry: the models registry for this run.
     :type model_registry: :class:`macsypy.registries.ModelRegistry` object.
     :return: the definitions to parse
     :rtype: list of :class:`macsypy.registries.DefinitionLocation` objects
     :raise ValueError: if a model name provided in models is not in model_registry.
     """
-    print("@@@@@@@@@@@@@@ get_def_to_detect", models)
     root, def_names = models
+    root = root.rstrip(os.path.sep)
     model_family = DefinitionLocation.root_name(root)
     model_loc = model_registry[model_family]
-    print("@@@@@@@@@@@@@@ root", root)
-    print("@@@@@@@@@@@@@@ def_names", def_names)
-    print("@@@@@@@@@@@@@@ model_family", model_family)
-    print("@@@@@@@@@@@@@@ model_loc", model_loc)
     if 'all' in [d.lower() for d in def_names]:
         if root == model_loc.name:
             root = None
-        print(f"@@@@@@@@@@@@ model_loc.get_all_definitions(root_def_name='{root}' @@@")
         def_to_detect = model_loc.get_all_definitions(root_def_name=root)
     else:
         def_to_detect = [model_loc.get_definition(f'{root}/{one_def}') for one_def in def_names]
