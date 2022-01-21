f60b2d1b9daa305d80ff7afad89fb2a47b504d23
Bertrand Néron <bneron@pasteur.fr>
2022-01-21 16:01:31 +0100

    :art: update copyrights date

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 # MacSyFinder - Detection of macromolecular systems in protein dataset  #
 #               using systems modelling and similarity search.          #
 # Authors: Sophie Abby, Bertrand Neron                                  #
-# Copyright (c) 2014-2021  Institut Pasteur (Paris) and CNRS.           #
+# Copyright (c) 2014-2022  Institut Pasteur (Paris) and CNRS.           #
 # See the COPYRIGHT file for details                                    #
 #                                                                       #
 # This file is part of MacSyFinder package.                             #
