4296da3f4a8a797297ecb2a1d2b84afaae032fa4
Bertrand Néron <bneron@pasteur.fr>
2022-02-16 10:32:46 +0100

    :pencil2: add footer in html doc
    to display the doc licensing

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -65,6 +65,11 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = ['_css/custom.css']
+
+
 # customize the latex output
 # it allow more level in nested list
 # (thx https://stackoverflow.com/questions/28454217/how-to-avoid-the-too-deeply-nested-error-when-creating-pdfs-with-sphinx)
